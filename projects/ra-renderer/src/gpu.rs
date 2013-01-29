e8a28184b495e06c8bf2ab058080dfc2fd7aecd8
SasakiSaki <galaster@foxmail.com>
2013-01-29 00:18:37 +0000

    ✨ Add wgpu surface clear-screen path in `ra-renderer`
    Initialize a desktop `GpuContext` and present a solid clear each frame over DX12/Vulkan/Metal backends.

---
+++ projects/ra-renderer/src/gpu.rs
@@ -0,0 +1,105 @@
+//! wgpu 设备 / 表面初始化（桌面路径）。
+
+use std::sync::Arc;
+
+use pollster::FutureExt as _;
+use ra_types::{RaError, RaResult};
+use winit::window::Window;
+
+pub struct GpuContext {
+    pub surface: wgpu::Surface<'static>,
+    pub device: wgpu::Device,
+    pub queue: wgpu::Queue,
+    pub config: wgpu::SurfaceConfiguration,
+    backend: wgpu::Backend,
+}
+
+impl GpuContext {
+    pub fn new(window: Arc<Window>) -> RaResult<Self> {
+        Self::new_async(window).block_on()
+    }
+
+    async fn new_async(window: Arc<Window>) -> RaResult<Self> {
+        let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::PRIMARY,
+            ..Default::default()
+        });
+
+        let surface = instance
+            .create_surface(window.clone())
+            .map_err(|e| RaError::Msg(format!("创建 wgpu 表面失败: {e}")))?;
+
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::HighPerformance,
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .await
+            .ok_or_else(|| RaError::Msg("未找到可用 GPU".into()))?;
+
+        let info = adapter.get_info();
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    label: Some("ra.device"),
+                    required_features: wgpu::Features::empty(),
+                    required_limits: wgpu::Limits::default(),
+                    memory_hints: Default::default(),
+                },
+                None,
+            )
+            .await
+            .map_err(|e| RaError::Msg(format!("创建 wgpu 设备失败: {e}")))?;
+
+        let caps = surface.get_capabilities(&adapter);
+        let format = caps
+            .formats
+            .iter()
+            .copied()
+            .find(|f| f.is_srgb())
+            .unwrap_or(caps.formats[0]);
+        let size = window.inner_size();
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format,
+            width: size.width.max(1),
+            height: size.height.max(1),
+            present_mode: wgpu::PresentMode::Fifo,
+            alpha_mode: caps.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+        };
+        surface.configure(&device, &config);
+
+        Ok(Self {
+            surface,
+            device,
+            queue,
+            config,
+            backend: info.backend,
+        })
+    }
+
+    pub fn resize(&mut self, width: u32, height: u32) {
+        let width = width.max(1);
+        let height = height.max(1);
+        if self.config.width == width && self.config.height == height {
+            return;
+        }
+        self.config.width = width;
+        self.config.height = height;
+        self.surface.configure(&self.device, &self.config);
+    }
+
+    pub fn backend_label(&self) -> &'static str {
+        match self.backend {
+            wgpu::Backend::Dx12 => "wgpu/dx12",
+            wgpu::Backend::Vulkan => "wgpu/vulkan",
+            wgpu::Backend::Metal => "wgpu/metal",
+            wgpu::Backend::Gl => "wgpu/gl",
+            wgpu::Backend::BrowserWebGpu => "wgpu/webgpu",
+            _ => "wgpu",
+        }
+    }
+}
--- projects/ra-renderer/src/lib.rs
+++ projects/ra-renderer/src/lib.rs
@@ -3,27 +3,107 @@
 //! 原生后端：DX12 / Vulkan / Metal。Wasm：WebGL2。
 //! 本 crate **故意不**实现 DirectDraw。
 
-use ra_types::GameEdition;
-use ra_world::World;
+mod gpu;
 
-#[derive(Debug, Default)]
+use std::sync::Arc;
+
+use ra_types::{GameEdition, RaResult};
+use ra_world::World;
+use winit::window::Window;
+
+use crate::gpu::GpuContext;
+
+/// 清屏底色（接近夜间战术图感觉，非最终主题）。
+const CLEAR_COLOR: wgpu::Color = wgpu::Color {
+    r: 0.04,
+    g: 0.06,
+    b: 0.09,
+    a: 1.0,
+};
+
 pub struct Renderer {
     pub frames: u64,
+    gpu: Option<GpuContext>,
 }
 
 impl Renderer {
     pub fn new() -> Self {
-        Self::default()
+        Self {
+            frames: 0,
+            gpu: None,
+        }
     }
 
-    /// 占位：等壳里接上 wgpu 表面后再画真帧。
-    pub fn draw_frame(&mut self, world: &World) {
-        let _ = world.edition;
+    /// 窗口就绪后绑定表面。可重复调用（忽略已绑定）。
+    pub fn attach_window(&mut self, window: Arc<Window>) -> RaResult<()> {
+        if self.gpu.is_some() {
+            return Ok(());
+        }
+        self.gpu = Some(GpuContext::new(window)?);
+        Ok(())
+    }
+
+    pub fn resize(&mut self, width: u32, height: u32) {
+        if let Some(gpu) = self.gpu.as_mut() {
+            gpu.resize(width, height);
+        }
+    }
+
+    /// 清屏一帧。世界参数留给后续精灵/地形通道。
+    pub fn draw_frame(&mut self, world: Option<&World>) {
+        if let Some(world) = world {
+            let _ = world.edition;
+        }
         self.frames = self.frames.wrapping_add(1);
+        let Some(gpu) = self.gpu.as_ref() else {
+            return;
+        };
+        let Ok(frame) = gpu.surface.get_current_texture() else {
+            return;
+        };
+        let view = frame
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = gpu
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("ra.clear"),
+            });
+        {
+            let _pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("ra.clear_pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(CLEAR_COLOR),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                timestamp_writes: None,
+                occlusion_query_set: None,
+            });
+        }
+        gpu.queue.submit(std::iter::once(encoder.finish()));
+        frame.present();
+    }
+
+    pub fn backend_name(&self) -> &'static str {
+        self.gpu
+            .as_ref()
+            .map(|g| g.backend_label())
+            .unwrap_or("wgpu(pending)")
     }
 
     pub fn backend_hint(edition: GameEdition) -> &'static str {
         let _ = edition;
         "wgpu"
+    }
+}
+
+impl Default for Renderer {
+    fn default() -> Self {
+        Self::new()
     }
 }
