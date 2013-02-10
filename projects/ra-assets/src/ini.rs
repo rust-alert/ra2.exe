c78055368ee7a536d8f24a80efce4f284a8434f4
SasakiSaki <galaster@foxmail.com>
2013-02-10 16:46:32 +0000

    ✨ Preserve INI value order for numbered pack sections
    Track insertion order in `IniSection` and add `numbered_section_concat` for IsoMapPack5-style payloads.

--- projects/ra-assets/src/ini.rs
+++ projects/ra-assets/src/ini.rs
@@ -1,4 +1,4 @@
-//! 最小 INI 读取，用于 rules/art 启动。
+//! 最小 INI 读取，用于 rules/art/地图启动。
 
 use std::collections::BTreeMap;
 
@@ -7,6 +7,8 @@ use ra_types::{RaError, RaResult};
 #[derive(Debug, Default, Clone)]
 pub struct IniSection {
     pub values: BTreeMap<String, String>,
+    /// 写入顺序，供 IsoMapPack 等按行拼接使用。
+    pub order: Vec<(String, String)>,
 }
 
 #[derive(Debug, Default, Clone)]
@@ -32,11 +34,11 @@ impl IniDocument {
                 continue;
             }
             if let Some((k, v)) = line.split_once('=') {
-                doc.sections
-                    .entry(current.clone())
-                    .or_default()
-                    .values
-                    .insert(k.trim().to_string(), v.trim().to_string());
+                let key = k.trim().to_string();
+                let val = v.trim().to_string();
+                let section = doc.sections.entry(current.clone()).or_default();
+                section.order.push((key.clone(), val.clone()));
+                section.values.insert(key, val);
             }
         }
         Ok(doc)
@@ -47,5 +49,42 @@ impl IniDocument {
             .get(section)
             .and_then(|s| s.values.get(key))
             .map(|s| s.as_str())
+    }
+
+    /// 将编号键（`1=` / `2=` …）按数值序拼接，用于 IsoMapPack5 / OverlayPack。
+    pub fn numbered_section_concat(&self, section: &str) -> Option<String> {
+        let sec = self.sections.get(section)?;
+        let mut pairs: Vec<(u32, &str)> = Vec::new();
+        for (k, v) in &sec.order {
+            if let Ok(n) = k.parse::<u32>() {
+                pairs.push((n, v.as_str()));
+            }
+        }
+        if pairs.is_empty() {
+            return None;
+        }
+        pairs.sort_by_key(|(n, _)| *n);
+        let mut out = String::new();
+        for (_, v) in pairs {
+            out.push_str(v);
+        }
+        Some(out)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn numbered_concat_sorts_numerically() {
+        let doc = IniDocument::parse(
+            b"[IsoMapPack5]\n10=C\n2=B\n1=A\n",
+        )
+        .unwrap();
+        assert_eq!(
+            doc.numbered_section_concat("IsoMapPack5").as_deref(),
+            Some("ABC")
+        );
     }
 }
