611f2a3a8bd5bbbe34d9c4ef1f69a95fb7ea49ed
SasakiSaki <galaster@foxmail.com>
2013-03-06 06:17:39 +0000

    ✨ Paint structure SHPs onto the boot map preview
    Resolve `[Structures]` art images with `NewTheater` filenames when needed and blit frame 0 after terrain objects.

--- projects/ra-desktop/src/main.rs
+++ projects/ra-desktop/src/main.rs
@@ -13,9 +13,9 @@ use std::sync::Arc;
 use ra_adaptor::{detect_edition, find_ci_file};
 use ra_assets::{IniDocument, Palette, ShpFile, TmpFile};
 use ra_map::{
-    compose_terrain_rgba, paint_cell_sprites, paint_overlay_markers, parse_tileset_ini,
-    theater_ini_name, theater_mix_names, theater_palette, theater_tmp_extension, MapInfo, Theater,
-    TileBlit,
+    compose_terrain_rgba, new_theater_shp_name, paint_cell_sprites, paint_overlay_markers,
+    parse_tileset_ini, theater_ini_name, theater_mix_names, theater_palette,
+    theater_tmp_extension, MapEntityKind, MapInfo, Theater, TileBlit,
 };
 use ra_renderer::{Renderer, RgbaImage};
 use ra_rules::load_rules;
@@ -255,16 +255,18 @@ fn load_map_terrain_preview(
         })
     };
     let terrain_painted = paint_terrain_objects(source, map, &mut image, &z_lookup);
+    let structure_painted = paint_structure_entities(source, map, &mut image, &z_lookup);
     let rgba = RgbaImage::new(image.width, image.height, image.pixels)?;
     Some((
         format!(
-            "map:{} cells={} drawn={} overlay#{} painted={} terrain_shp#{} {}x{}",
+            "map:{} cells={} drawn={} overlay#{} painted={} terrain_shp#{} struct_shp#{} {}x{}",
             map.name,
             map.cells.len(),
             image.drawn,
             map.overlays.len(),
             overlay_painted,
             terrain_painted,
+            structure_painted,
             rgba.width,
             rgba.height
         ),
@@ -342,6 +344,113 @@ fn paint_terrain_objects(
         };
         blit_cache.insert(image_key, blit.clone());
         items.push((obj.x, obj.y, blit));
+    }
+
+    paint_cell_sprites(image, &items, |x, y| {
+        z_lookup.get(&(x, y)).copied().unwrap_or(0)
+    })
+}
+
+/// 叠画 `[Structures]`：优先 `NewTheater` 文件名，否则普通 `.shp`。
+fn paint_structure_entities(
+    source: &GameAssetSource,
+    map: &MapInfo,
+    image: &mut ra_map::TerrainImage,
+    z_lookup: &HashMap<(u16, u16), u8>,
+) -> usize {
+    let structures: Vec<_> = map
+        .entities
+        .iter()
+        .filter(|e| e.kind == MapEntityKind::Structure)
+        .collect();
+    if structures.is_empty() {
+        return 0;
+    }
+    let art = source
+        .vfs
+        .read("art.ini")
+        .and_then(|b| IniDocument::parse(&b).ok());
+    let obj_pal = source
+        .vfs
+        .read("unittem.pal")
+        .and_then(|b| Palette::parse(&b).ok())
+        .or_else(|| {
+            source
+                .vfs
+                .read(theater_palette(map.theater))
+                .and_then(|b| Palette::parse(&b).ok())
+        });
+    let Some(obj_pal) = obj_pal else {
+        return 0;
+    };
+
+    let mut shp_cache: HashMap<String, ShpFile> = HashMap::new();
+    let mut blit_cache: HashMap<String, TileBlit> = HashMap::new();
+    let mut items: Vec<(u16, u16, TileBlit)> = Vec::new();
+
+    for ent in structures {
+        let image_key = art
+            .as_ref()
+            .and_then(|a| a.get(&ent.type_id, "Image"))
+            .unwrap_or(ent.type_id.as_str())
+            .to_ascii_uppercase();
+        if let Some(blit) = blit_cache.get(&image_key) {
+            items.push((ent.x, ent.y, blit.clone()));
+            continue;
+        }
+        let new_theater = art
+            .as_ref()
+            .and_then(|a| a.get(&ent.type_id, "NewTheater"))
+            .is_some_and(|v| v.eq_ignore_ascii_case("yes"));
+        let candidates = if new_theater {
+            vec![
+                new_theater_shp_name(&image_key, map.theater),
+                format!("{}.shp", image_key.to_ascii_lowercase()),
+            ]
+        } else {
+            vec![
+                format!("{}.shp", image_key.to_ascii_lowercase()),
+                new_theater_shp_name(&image_key, map.theater),
+            ]
+        };
+
+        let mut loaded: Option<String> = None;
+        for file in &candidates {
+            if shp_cache.contains_key(file) {
+                loaded = Some(file.clone());
+                break;
+            }
+            let Some(bytes) = source.vfs.read(file) else {
+                continue;
+            };
+            let Ok(shp) = ShpFile::parse(&bytes) else {
+                continue;
+            };
+            shp_cache.insert(file.clone(), shp);
+            loaded = Some(file.clone());
+            break;
+        }
+        let Some(file) = loaded else {
+            continue;
+        };
+        let Some(shp) = shp_cache.get(&file) else {
+            continue;
+        };
+        let Some(frame) = shp.frames.first() else {
+            continue;
+        };
+        if frame.frame_width == 0 || frame.frame_height == 0 {
+            continue;
+        }
+        let blit = TileBlit {
+            width: u32::from(frame.frame_width),
+            height: u32::from(frame.frame_height),
+            offset_x: i32::from(frame.frame_x),
+            offset_y: i32::from(frame.frame_y),
+            rgba: frame.to_rgba(&obj_pal),
+        };
+        blit_cache.insert(image_key, blit.clone());
+        items.push((ent.x, ent.y, blit));
     }
 
     paint_cell_sprites(image, &items, |x, y| {
