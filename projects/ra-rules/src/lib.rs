8a847a8dedaf27375203a963795f831bfb19114d
SasakiSaki <galaster@foxmail.com>
2013-03-11 18:13:41 +0000

    ✨ Parse `[OverlayTypes]` into `RulesDb`
    Add `OverlayTypeRegistry` so overlay pack ids can resolve to rules type names such as `TIB01` or `GAWALL`.

--- projects/ra-rules/src/lib.rs
+++ projects/ra-rules/src/lib.rs
@@ -1,23 +1,30 @@
 //! 按版本对应的 INI 名加载规则。
 
+mod overlay_types;
+
 use ra_adaptor::ResourceChain;
 use ra_assets::IniDocument;
 use ra_types::{AssetSource, GameEdition, RaResult};
+
+pub use overlay_types::OverlayTypeRegistry;
 
 #[derive(Debug, Clone)]
 pub struct RulesDb {
     pub edition: GameEdition,
     pub rules: IniDocument,
     pub art: IniDocument,
+    pub overlay_types: OverlayTypeRegistry,
 }
 
 pub fn load_rules(source: &dyn AssetSource, edition: GameEdition) -> RaResult<RulesDb> {
     let chain = ResourceChain::for_edition(edition);
     let rules = IniDocument::parse(&source.read(chain.rules_ini)?)?;
     let art = IniDocument::parse(&source.read(chain.art_ini)?)?;
+    let overlay_types = OverlayTypeRegistry::from_rules(&rules);
     Ok(RulesDb {
         edition,
         rules,
         art,
+        overlay_types,
     })
 }
---
+++ projects/ra-rules/src/overlay_types.rs
@@ -0,0 +1,72 @@
+//! 从 `rules.ini` 的 `[OverlayTypes]` 建立 id → 名称表。
+
+use ra_assets::IniDocument;
+
+/// Overlay 类型注册表（按规则编号键）。
+#[derive(Debug, Clone, Default)]
+pub struct OverlayTypeRegistry {
+    /// `overlay_id` → 类型名（大写）。
+    names: Vec<Option<String>>,
+}
+
+impl OverlayTypeRegistry {
+    /// 解析 `[OverlayTypes]`；键为十进制 id。
+    pub fn from_rules(rules: &IniDocument) -> Self {
+        let Some(section) = rules.sections.get("OverlayTypes") else {
+            return Self::default();
+        };
+        let mut max_id = 0usize;
+        let mut pairs: Vec<(usize, String)> = Vec::new();
+        for (key, value) in &section.order {
+            let Ok(id) = key.parse::<usize>() else {
+                continue;
+            };
+            let name = value.trim();
+            if name.is_empty() {
+                continue;
+            }
+            max_id = max_id.max(id);
+            pairs.push((id, name.to_ascii_uppercase()));
+        }
+        let mut names = vec![None; max_id.saturating_add(1)];
+        for (id, name) in pairs {
+            if id < names.len() {
+                names[id] = Some(name);
+            }
+        }
+        Self { names }
+    }
+
+    pub fn len(&self) -> usize {
+        self.names.iter().filter(|n| n.is_some()).count()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    pub fn name(&self, id: u8) -> Option<&str> {
+        self.names
+            .get(usize::from(id))
+            .and_then(|n| n.as_deref())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_sparse_ids() {
+        let doc = IniDocument::parse(
+            b"[OverlayTypes]\n3=GAWALL\n105=TIB01\n1=GASAND\n",
+        )
+        .unwrap();
+        let reg = OverlayTypeRegistry::from_rules(&doc);
+        assert_eq!(reg.len(), 3);
+        assert_eq!(reg.name(1), Some("GASAND"));
+        assert_eq!(reg.name(3), Some("GAWALL"));
+        assert_eq!(reg.name(105), Some("TIB01"));
+        assert_eq!(reg.name(2), None);
+    }
+}
