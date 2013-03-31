632b4405dce9c3c94bf61d8cd5b51786f9ee68ab
SasakiSaki <galaster@foxmail.com>
2013-03-31 09:33:48 +0000

    ✨ Parse rules `[Colors]` schemes in `ra-rules`
    Store named HSV triples and resolve each house `Color=` entry so boot remapping can follow retail faction schemes.

---
+++ projects/ra-rules/src/color_schemes.rs
@@ -0,0 +1,95 @@
+//! rules `[Colors]`：名称 → HSV 方案。
+
+use std::collections::HashMap;
+
+use ra_assets::{Hsv, IniDocument};
+
+/// 零售 `[Colors]` 表。
+#[derive(Debug, Clone, Default)]
+pub struct ColorSchemes {
+    by_name: HashMap<String, Hsv>,
+}
+
+impl ColorSchemes {
+    /// 从 rules 文档解析；缺节则空表。
+    pub fn from_rules(doc: &IniDocument) -> Self {
+        let mut by_name = HashMap::new();
+        let Some(sec) = doc.sections.get("Colors") else {
+            return Self { by_name };
+        };
+        for (name, value) in &sec.order {
+            if let Some(hsv) = parse_hsv(value) {
+                by_name.insert(name.to_ascii_uppercase(), hsv);
+            }
+        }
+        Self { by_name }
+    }
+
+    pub fn get(&self, name: &str) -> Option<Hsv> {
+        self.by_name.get(&name.to_ascii_uppercase()).copied()
+    }
+
+    /// 阵营节 `Color=` → HSV；中立等跳过。
+    pub fn hsv_for_house(&self, rules: &IniDocument, house: &str) -> Option<Hsv> {
+        let up = house.to_ascii_uppercase();
+        if matches!(up.as_str(), "NEUTRAL" | "SPECIAL" | "CIVILIAN") {
+            return None;
+        }
+        // 节名大小写与零售一致时直接取；否则扫一遍。
+        let scheme = rules
+            .get(house, "Color")
+            .or_else(|| {
+                rules.sections.keys().find_map(|k| {
+                    if k.eq_ignore_ascii_case(house) {
+                        rules.get(k, "Color")
+                    } else {
+                        None
+                    }
+                })
+            })?;
+        self.get(scheme)
+    }
+
+    pub fn len(&self) -> usize {
+        self.by_name.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.by_name.is_empty()
+    }
+}
+
+fn parse_hsv(value: &str) -> Option<Hsv> {
+    let mut parts = value.split(',').map(str::trim);
+    let h: u8 = parts.next()?.parse().ok()?;
+    let s: u8 = parts.next()?.parse().ok()?;
+    let v: u8 = parts.next()?.parse().ok()?;
+    Some(Hsv { h, s, v })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_colors_and_house() {
+        let doc = IniDocument::parse(
+            b"[Colors]\nGold=41,240,230\nDarkRed=0,230,255\n\
+[Americans]\nColor=Gold\n\
+[Russians]\nColor=DarkRed\n\
+[Neutral]\nColor=Grey\n",
+        )
+        .unwrap();
+        let schemes = ColorSchemes::from_rules(&doc);
+        assert_eq!(schemes.len(), 2);
+        assert_eq!(
+            schemes.hsv_for_house(&doc, "Americans"),
+            Some(Hsv {
+                h: 41,
+                s: 240,
+                v: 230
+            })
+        );
+        assert!(schemes.hsv_for_house(&doc, "Neutral").is_none());
+    }
+}
--- projects/ra-rules/src/lib.rs
+++ projects/ra-rules/src/lib.rs
@@ -1,11 +1,13 @@
 //! 按版本对应的 INI 名加载规则。
 
+mod color_schemes;
 mod overlay_types;
 
 use ra_adaptor::ResourceChain;
 use ra_assets::IniDocument;
 use ra_types::{AssetSource, GameEdition, RaResult};
 
+pub use color_schemes::ColorSchemes;
 pub use overlay_types::OverlayTypeRegistry;
 
 #[derive(Debug, Clone)]
@@ -14,6 +16,7 @@ pub struct RulesDb {
     pub rules: IniDocument,
     pub art: IniDocument,
     pub overlay_types: OverlayTypeRegistry,
+    pub color_schemes: ColorSchemes,
 }
 
 pub fn load_rules(source: &dyn AssetSource, edition: GameEdition) -> RaResult<RulesDb> {
@@ -21,10 +24,12 @@ pub fn load_rules(source: &dyn AssetSource, edition: GameEdition) -> RaResult<Ru
     let rules = IniDocument::parse(&source.read(chain.rules_ini)?)?;
     let art = IniDocument::parse(&source.read(chain.art_ini)?)?;
     let overlay_types = OverlayTypeRegistry::from_rules(&rules);
+    let color_schemes = ColorSchemes::from_rules(&rules);
     Ok(RulesDb {
         edition,
         rules,
         art,
         overlay_types,
+        color_schemes,
     })
 }
