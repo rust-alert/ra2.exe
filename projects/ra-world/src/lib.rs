d32f2a499592045439bb30ae03783481f0284b0a
SasakiSaki <galaster@foxmail.com>
2013-04-16 08:00:34 +0000

    ✨ Advance mobile entities toward nearest waypoint
    Accumulate Speed each tick and step one map cell when enough move points gather so seeded units begin deterministic locomotion.

--- projects/ra-world/src/lib.rs
+++ projects/ra-world/src/lib.rs
@@ -4,6 +4,9 @@ use ra_map::{MapEntityKind, MapInfo};
 use ra_rules::{RulesDb, TechnoKind};
 use ra_types::{GameEdition, PlayerId};
 
+/// 走一格所需的移动点（预览用常量，非零售精确换算）。
+pub const CELL_MOVE_COST: u32 = 64;
+
 /// 世界中的一个已放置实体（由地图播种，后续仿真就地改）。
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct WorldEntity {
@@ -19,6 +22,11 @@ pub struct WorldEntity {
     pub max_health: u32,
     pub speed: u32,
     pub techno_kind: Option<TechnoKind>,
+    /// 简易移动目标格；无航点时为 `None`。
+    pub target_x: Option<u16>,
+    pub target_y: Option<u16>,
+    /// 累积移动点（每 tick += Speed）。
+    pub move_accum: u32,
 }
 
 #[derive(Debug, Clone)]
@@ -40,6 +48,12 @@ impl World {
                 let tt = rules.techno_types.get(&e.type_id);
                 let max_health = tt.map(|t| t.strength).unwrap_or(1).max(1);
                 let health = (u64::from(max_health) * u64::from(e.health) / 256) as u32;
+                let speed = tt.map(|t| t.speed).unwrap_or(0);
+                let (target_x, target_y) = if is_mobile(e.kind) && speed > 0 {
+                    nearest_waypoint(&map, e.x, e.y)
+                } else {
+                    (None, None)
+                };
                 WorldEntity {
                     kind: e.kind,
                     owner: e.owner.clone(),
@@ -50,8 +64,11 @@ impl World {
                     sub_cell: e.sub_cell,
                     health,
                     max_health,
-                    speed: tt.map(|t| t.speed).unwrap_or(0),
+                    speed,
                     techno_kind: tt.map(|t| t.kind),
+                    target_x,
+                    target_y,
+                    move_accum: 0,
                 }
             })
             .collect();
@@ -69,6 +86,24 @@ impl World {
 
     pub fn advance_tick(&mut self) {
         self.tick = self.tick.wrapping_add(1);
+        for e in &mut self.entities {
+            if e.speed == 0 || !is_mobile(e.kind) {
+                continue;
+            }
+            let (Some(tx), Some(ty)) = (e.target_x, e.target_y) else {
+                continue;
+            };
+            if e.x == tx && e.y == ty {
+                continue;
+            }
+            e.move_accum = e.move_accum.saturating_add(e.speed);
+            while e.move_accum >= CELL_MOVE_COST {
+                e.move_accum -= CELL_MOVE_COST;
+                if !step_toward(e, tx, ty) {
+                    break;
+                }
+            }
+        }
         self.rehash();
     }
 
@@ -106,11 +141,54 @@ impl World {
     }
 }
 
+fn is_mobile(kind: MapEntityKind) -> bool {
+    matches!(
+        kind,
+        MapEntityKind::Unit | MapEntityKind::Infantry | MapEntityKind::Aircraft
+    )
+}
+
+fn nearest_waypoint(map: &MapInfo, x: u16, y: u16) -> (Option<u16>, Option<u16>) {
+    let Some(best) = map.waypoints.iter().min_by_key(|w| {
+        let dx = i32::from(w.x) - i32::from(x);
+        let dy = i32::from(w.y) - i32::from(y);
+        dx * dx + dy * dy
+    }) else {
+        return (None, None);
+    };
+    (Some(best.x), Some(best.y))
+}
+
+/// 向目标迈一格；已到达则返回 `false`。
+fn step_toward(e: &mut WorldEntity, tx: u16, ty: u16) -> bool {
+    if e.x == tx && e.y == ty {
+        return false;
+    }
+    let dx = i32::from(tx) - i32::from(e.x);
+    let dy = i32::from(ty) - i32::from(e.y);
+    if dx.abs() >= dy.abs() {
+        if dx > 0 {
+            e.x = e.x.saturating_add(1);
+            e.facing = 0;
+        } else {
+            e.x = e.x.saturating_sub(1);
+            e.facing = 128;
+        }
+    } else if dy > 0 {
+        e.y = e.y.saturating_add(1);
+        e.facing = 64;
+    } else {
+        e.y = e.y.saturating_sub(1);
+        e.facing = 192;
+    }
+    true
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use ra_assets::IniDocument;
-    use ra_map::MapEntity;
+    use ra_map::{MapEntity, Waypoint};
     use ra_rules::{
         ColorSchemes, OverlayTypeRegistry, RulesDb, TechnoKind, TechnoTypeRegistry,
     };
@@ -119,7 +197,7 @@ mod tests {
     fn rules_with_mtnk() -> RulesDb {
         let doc = IniDocument::parse(
             b"[VehicleTypes]\n0=MTNK\n\
-[MTNK]\nStrength=400\nSpeed=6\nSight=6\nCost=800\nArmor=heavy\n",
+[MTNK]\nStrength=400\nSpeed=64\nSight=6\nCost=800\nArmor=heavy\n",
         )
         .unwrap();
         RulesDb {
@@ -151,8 +229,38 @@ mod tests {
         let e = &world.entities[0];
         assert_eq!(e.max_health, 400);
         assert_eq!(e.health, 400);
-        assert_eq!(e.speed, 6);
+        assert_eq!(e.speed, 64);
         assert_eq!(e.techno_kind, Some(TechnoKind::Vehicle));
         assert_eq!(world.bound_techno_count(), 1);
+    }
+
+    #[test]
+    fn advances_toward_waypoint() {
+        let rules = rules_with_mtnk();
+        let mut map = MapInfo::empty(GameEdition::Ra2, "t");
+        map.waypoints.push(Waypoint {
+            index: 0,
+            x: 12,
+            y: 20,
+        });
+        map.entities.push(MapEntity {
+            kind: MapEntityKind::Unit,
+            owner: "Americans".into(),
+            type_id: "MTNK".into(),
+            health: 256,
+            x: 10,
+            y: 20,
+            facing: 0,
+            sub_cell: 0,
+        });
+        let mut world = World::new(GameEdition::Ra2, &rules, map);
+        assert_eq!(world.entities[0].target_x, Some(12));
+        // Speed=64 → 每 tick 正好走一格。
+        world.advance_tick();
+        assert_eq!(world.entities[0].x, 11);
+        world.advance_tick();
+        assert_eq!(world.entities[0].x, 12);
+        world.advance_tick();
+        assert_eq!(world.entities[0].x, 12);
     }
 }
