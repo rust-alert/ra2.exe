b16a6ba21dab96b99c7d733c32179ac0a311fb13
SasakiSaki <galaster@foxmail.com>
2013-04-18 12:49:53 +0000

    ✨ Keep mobiles from stacking with occupancy-aware paths
    Block other mobile cells during `repath_at` and repath when the next step is taken.

--- projects/ra-world/src/lib.rs
+++ projects/ra-world/src/lib.rs
@@ -45,7 +45,7 @@ pub struct World {
 impl World {
     pub fn new(edition: GameEdition, rules: &RulesDb, map: MapInfo) -> Self {
         let pass_grid = PassGrid::from_map(&map);
-        let mut entities: Vec<WorldEntity> = map
+        let entities: Vec<WorldEntity> = map
             .entities
             .iter()
             .map(|e| {
@@ -77,9 +77,6 @@ impl World {
                 }
             })
             .collect();
-        for e in &mut entities {
-            repath(e, &pass_grid);
-        }
         let mut world = Self {
             edition,
             tick: 0,
@@ -89,33 +86,54 @@ impl World {
             local_player: PlayerId(0),
             state_hash: 0,
         };
+        for i in 0..world.entities.len() {
+            repath_at(&mut world.entities, i, &world.pass_grid);
+        }
         world.rehash();
         world
     }
 
     pub fn advance_tick(&mut self) {
         self.tick = self.tick.wrapping_add(1);
-        for e in &mut self.entities {
-            if e.speed == 0 || !is_mobile(e.kind) {
+        let n = self.entities.len();
+        for i in 0..n {
+            if self.entities[i].speed == 0 || !is_mobile(self.entities[i].kind) {
                 continue;
             }
-            let (Some(tx), Some(ty)) = (e.target_x, e.target_y) else {
+            let (Some(tx), Some(ty)) = (self.entities[i].target_x, self.entities[i].target_y)
+            else {
                 continue;
             };
-            if e.x == tx && e.y == ty {
-                e.path.clear();
+            if self.entities[i].x == tx && self.entities[i].y == ty {
+                self.entities[i].path.clear();
                 continue;
             }
-            e.move_accum = e.move_accum.saturating_add(e.speed);
-            while e.move_accum >= CELL_MOVE_COST {
-                e.move_accum -= CELL_MOVE_COST;
-                if e.path.is_empty() {
-                    repath(e, &self.pass_grid);
-                    if e.path.is_empty() {
+            self.entities[i].move_accum = self.entities[i]
+                .move_accum
+                .saturating_add(self.entities[i].speed);
+            while self.entities[i].move_accum >= CELL_MOVE_COST {
+                self.entities[i].move_accum -= CELL_MOVE_COST;
+                if self.entities[i].path.is_empty() {
+                    repath_at(&mut self.entities, i, &self.pass_grid);
+                    if self.entities[i].path.is_empty() {
                         break;
                     }
                 }
-                if !step_along_path(e) {
+                let Some((nx, ny)) = self.entities[i].path.first().copied() else {
+                    break;
+                };
+                if cell_occupied_by_other(&self.entities, i, nx, ny) {
+                    // 下一格被占：清路再寻，仍堵则本 tick 停步。
+                    self.entities[i].path.clear();
+                    repath_at(&mut self.entities, i, &self.pass_grid);
+                    let Some((nx2, ny2)) = self.entities[i].path.first().copied() else {
+                        break;
+                    };
+                    if cell_occupied_by_other(&self.entities, i, nx2, ny2) {
+                        break;
+                    }
+                }
+                if !step_along_path(&mut self.entities[i]) {
                     break;
                 }
             }
@@ -175,21 +193,35 @@ fn nearest_waypoint(map: &MapInfo, x: u16, y: u16) -> (Option<u16>, Option<u16>)
     (Some(best.x), Some(best.y))
 }
 
-fn repath(e: &mut WorldEntity, grid: &PassGrid) {
-    e.path.clear();
-    let (Some(tx), Some(ty)) = (e.target_x, e.target_y) else {
+fn cell_occupied_by_other(entities: &[WorldEntity], self_i: usize, x: u16, y: u16) -> bool {
+    entities.iter().enumerate().any(|(j, o)| {
+        j != self_i && is_mobile(o.kind) && o.x == x && o.y == y
+    })
+}
+
+/// 寻路时把其它移动单位占格封死；起点与目标临时放开。
+fn repath_at(entities: &mut [WorldEntity], i: usize, grid: &PassGrid) {
+    entities[i].path.clear();
+    let (Some(tx), Some(ty)) = (entities[i].target_x, entities[i].target_y) else {
         return;
     };
+    let (sx, sy) = (entities[i].x, entities[i].y);
     let mut g = grid.clone();
-    // 允许离开当前格（可能与建筑重叠的边界情况）。
-    g.set_passable(e.x, e.y, true);
-    let Some(mut path) = g.find_path(e.x, e.y, tx, ty) else {
+    for (j, o) in entities.iter().enumerate() {
+        if j != i && is_mobile(o.kind) {
+            g.set_passable(o.x, o.y, false);
+        }
+    }
+    // 允许离开当前格；目标格临时放开以便朝共享航点规划（迈入仍受占格检查）。
+    g.set_passable(sx, sy, true);
+    g.set_passable(tx, ty, true);
+    let Some(mut path) = g.find_path(sx, sy, tx, ty) else {
         return;
     };
-    if path.first() == Some(&(e.x, e.y)) {
+    if path.first() == Some(&(sx, sy)) {
         path.remove(0);
     }
-    e.path = path;
+    entities[i].path = path;
 }
 
 /// 沿 `path` 迈一格；无路则返回 `false`。
@@ -339,5 +371,52 @@ mod tests {
         }
         assert_eq!(world.entities[1].x, 14);
         assert_eq!(world.entities[1].y, 10);
+    }
+
+    #[test]
+    fn mobiles_detour_around_each_other() {
+        let rules = rules_with_mtnk();
+        let mut map = map_with_size();
+        map.waypoints.push(Waypoint {
+            index: 0,
+            x: 14,
+            y: 10,
+        });
+        // 挡在直线上的静止单位（Speed=0 用建筑外的占格：另一辆坦克无目标则不移动）。
+        map.entities.push(MapEntity {
+            kind: MapEntityKind::Unit,
+            owner: "Americans".into(),
+            type_id: "MTNK".into(),
+            health: 256,
+            x: 12,
+            y: 10,
+            facing: 0,
+            sub_cell: 0,
+        });
+        map.entities.push(MapEntity {
+            kind: MapEntityKind::Unit,
+            owner: "Americans".into(),
+            type_id: "MTNK".into(),
+            health: 256,
+            x: 10,
+            y: 10,
+            facing: 0,
+            sub_cell: 0,
+        });
+        let mut world = World::new(GameEdition::Ra2, &rules, map);
+        // 两车都朝同一航点；后者路径不得踩前者当前格。
+        assert!(!world.entities[1].path.is_empty());
+        assert!(!world.entities[1]
+            .path
+            .iter()
+            .any(|&(x, y)| x == world.entities[0].x && y == world.entities[0].y));
+        for _ in 0..40 {
+            world.advance_tick();
+        }
+        // 至少一车抵达或贴近航点；且不同时占同一格。
+        let a = (world.entities[0].x, world.entities[0].y);
+        let b = (world.entities[1].x, world.entities[1].y);
+        assert_ne!(a, b);
+        assert!(a == (14, 10) || b == (14, 10) || a.0.max(b.0) >= 13);
     }
 }
