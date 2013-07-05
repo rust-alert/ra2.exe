d23581d062ad2cc63e2795406dae5362bea46dba
SasakiSaki <galaster@foxmail.com>
2013-07-05 03:04:30 +0000

    📝 Add Alpha headless scripts for move ignore and pause
    Cover cell movement, out-of-bounds command ignore, and victory pause without GPU.

--- projects/ra-testing/src/headless.rs
+++ projects/ra-testing/src/headless.rs
@@ -113,7 +113,11 @@ mod tests {
         let mut first = standard_duel();
         let mut second = standard_duel();
         for case in [&mut first, &mut second] {
-            case.command(GameCommand::MoveTo { entity_index: 0, x: 5, y: 8 });
+            case.command(GameCommand::MoveTo {
+                entity_index: 0,
+                x: 5,
+                y: 8,
+            });
             case.advance(3);
         }
         let a = first.observe();
@@ -121,5 +125,55 @@ mod tests {
         assert_eq!(a.tick, b.tick);
         assert_eq!(a.state_hash, b.state_hash);
         assert_eq!(a.snapshot.units[0].x, b.snapshot.units[0].x);
+    }
+
+    #[test]
+    fn move_script_advances_unit_cell() {
+        let mut case = standard_duel();
+        case.command(GameCommand::MoveTo {
+            entity_index: 0,
+            x: 6,
+            y: 8,
+        });
+        case.advance(4);
+        let result = case.observe();
+        assert_eq!(result.snapshot.units[0].x, 6);
+        assert_eq!(result.snapshot.units[0].y, 8);
+        assert!(result.outcome.is_none());
+    }
+
+    #[test]
+    fn out_of_bounds_commands_are_ignored_without_panic() {
+        let mut case = standard_duel();
+        case.command(GameCommand::MoveTo {
+            entity_index: 99,
+            x: 1,
+            y: 1,
+        });
+        case.command(GameCommand::Attack {
+            attacker_index: 0,
+            target_index: 99,
+        });
+        case.advance(2);
+        let result = case.observe();
+        assert_eq!(result.snapshot.units[0].x, 4);
+        assert!(!result.snapshot.units[1].dead);
+        assert!(result.outcome.is_none());
+    }
+
+    #[test]
+    fn victory_pauses_further_ticks() {
+        let mut case = standard_duel();
+        case.command(GameCommand::Attack {
+            attacker_index: 0,
+            target_index: 1,
+        });
+        case.advance(64);
+        let after_win = case.observe();
+        assert!(after_win.outcome.is_some());
+        let tick_at_win = after_win.tick;
+        case.advance(8);
+        assert_eq!(case.observe().tick, tick_at_win);
+        assert!(case.session.paused);
     }
 }
