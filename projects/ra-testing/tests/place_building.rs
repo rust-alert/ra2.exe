17d36ae82abb94e7a4640a3ed518e76b3b1316ca
aster <aster@vers.site>
2013-07-20 21:34:56 +0000

    🧪 Add `yard_open` session place-building coverage
    Drive `PlaceBuilding` through `Session` with a seeded construction yard so funds deduction and power spawn stay on the product tick path.

---
+++ projects/ra-testing/tests/place_building.rs
@@ -0,0 +1,26 @@
+//! 经 Session 放置建筑。
+
+use ra_map::MapEntityKind;
+use ra_testing::{alpha_skirmish_v1, yard_open};
+use ra_types::PlayerId;
+use ra_world::GameCommand;
+
+#[test]
+fn place_power_through_session_deducts_funds() {
+    let mut case = yard_open();
+    let slice = alpha_skirmish_v1();
+    let before = case.session.world.house_funds(slice.human_house).expect("应有资金");
+    case.command(GameCommand::PlaceBuilding {
+        player: PlayerId(0),
+        type_id: "GAPOWR".into(),
+        x: 6,
+        y: 4,
+    });
+    case.advance(1);
+    assert!(case.session.world.last_rejects().is_empty());
+    assert_eq!(case.session.world.house_funds(slice.human_house), Some(before - 600));
+    assert_eq!(case.session.world.entities.len(), 2);
+    assert_eq!(case.session.world.entities[1].kind, MapEntityKind::Structure);
+    assert_eq!(case.session.world.entities[1].type_id, "GAPOWR");
+    assert_eq!(case.session.world.players[0].power_output, 200);
+}
