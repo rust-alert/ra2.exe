f7d8e97561b2c389bc623eb03b778d2191186d16
aster <aster@vers.site>
2013-08-03 23:08:46 +0000

    ✨ Extend test status sidecar with economy fields
    Write local funds, power, produce queue, and last reject into the harness status file, and teach `TestStatus` to parse `funds>=N` / `queue!=none` expectations.

--- projects/ra-testing/tests/status_sidecar.rs
+++ projects/ra-testing/tests/status_sidecar.rs
@@ -9,6 +9,12 @@ outcome=victory:Americans
 paused=true
 selected=0,2
 entities=2
+funds=9400
+power_output=200
+power_drain=50
+low_power=false
+queue=E1:12
+last_reject=QueueFull
 ";
     let s = TestStatus::parse(text).unwrap();
     assert_eq!(s.tick, 12);
@@ -17,7 +23,15 @@ entities=2
     assert!(s.paused);
     assert_eq!(s.selected, vec![0, 2]);
     assert_eq!(s.entities, 2);
+    assert_eq!(s.funds, 9400);
+    assert_eq!(s.power_output, 200);
+    assert_eq!(s.power_drain, 50);
+    assert!(!s.low_power);
+    assert_eq!(s.queue, "E1:12");
+    assert_eq!(s.last_reject, "QueueFull");
     assert!(s.matches_expect("tick>=1"));
+    assert!(s.matches_expect("funds>=1000"));
+    assert!(s.matches_expect("queue!=none"));
     assert!(s.matches_expect("outcome!=none"));
     assert!(s.matches_expect("paused=true"));
 }
