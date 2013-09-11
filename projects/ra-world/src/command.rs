53d243eb37ffe53efccafcfe65b300e76faad95d
aster <aster@vers.site>
2013-09-11 03:32:50 +0000

    ♻️ Move command application into `command`
    Keep `lib.rs` focused on `World` construction public queries and the fixed `advance_tick` system order.

--- projects/ra-world/src/command.rs
+++ projects/ra-world/src/command.rs
@@ -228,3 +228,250 @@ pub fn decode_commands(bytes: &[u8]) -> Option<Vec<GameCommand>> {
     }
     Some(out)
 }
+
+impl crate::World {
+    pub(crate) fn apply_commands(&mut self, cmds: &[GameCommand]) {
+        use ra_assets::TechnoKind;
+        use ra_map::MapEntityKind;
+
+        use crate::navigation::{is_mobile, repath_at};
+        use crate::reject::CommandRejectReason;
+        use crate::rules::{
+            building_power_delta, deploy_into_type, full_verses, is_construction_yard, is_production_factory,
+            requires_power_plant,
+        };
+        use crate::{PRODUCE_TICKS, WorldEntity};
+
+        for (command_index, cmd) in cmds.iter().enumerate() {
+            match *cmd {
+                GameCommand::MoveTo { entity_index, x, y } => {
+                    if entity_index >= self.entities.len() {
+                        self.reject(command_index, CommandRejectReason::EntityNotFound);
+                        continue;
+                    }
+                    if self.entities[entity_index].dead {
+                        self.reject(command_index, CommandRejectReason::EntityDead);
+                        continue;
+                    }
+                    if !is_mobile(self.entities[entity_index].kind) {
+                        self.reject(command_index, CommandRejectReason::NotMobile);
+                        continue;
+                    }
+                    let e = &mut self.entities[entity_index];
+                    e.attack_target = None;
+                    e.target_x = Some(x);
+                    e.target_y = Some(y);
+                    e.path.clear();
+                    e.move_accum = 0;
+                    repath_at(&mut self.entities, entity_index, &self.pass_grid);
+                }
+                GameCommand::Attack { attacker_index, target_index } => {
+                    if attacker_index >= self.entities.len() || target_index >= self.entities.len() {
+                        self.reject(command_index, CommandRejectReason::EntityNotFound);
+                        continue;
+                    }
+                    if attacker_index == target_index {
+                        self.reject(command_index, CommandRejectReason::InvalidTarget);
+                        continue;
+                    }
+                    if self.entities[attacker_index].dead {
+                        self.reject(command_index, CommandRejectReason::EntityDead);
+                        continue;
+                    }
+                    if self.entities[target_index].dead {
+                        self.reject(command_index, CommandRejectReason::InvalidTarget);
+                        continue;
+                    }
+                    if !is_mobile(self.entities[attacker_index].kind) {
+                        self.reject(command_index, CommandRejectReason::NotMobile);
+                        continue;
+                    }
+                    let (tx, ty) = (self.entities[target_index].x, self.entities[target_index].y);
+                    let a = &mut self.entities[attacker_index];
+                    a.attack_target = Some(target_index);
+                    a.target_x = Some(tx);
+                    a.target_y = Some(ty);
+                    a.path.clear();
+                    a.move_accum = 0;
+                    repath_at(&mut self.entities, attacker_index, &self.pass_grid);
+                }
+                GameCommand::Deploy { entity_index } => {
+                    if entity_index >= self.entities.len() {
+                        self.reject(command_index, CommandRejectReason::EntityNotFound);
+                        continue;
+                    }
+                    if self.entities[entity_index].dead {
+                        self.reject(command_index, CommandRejectReason::EntityDead);
+                        continue;
+                    }
+                    let Some(building_type) = deploy_into_type(&self.entities[entity_index].type_id)
+                    else {
+                        self.reject(command_index, CommandRejectReason::CannotDeploy);
+                        continue;
+                    };
+                    let armor = self.techno_types.get(building_type).map(|t| t.armor.clone()).unwrap_or_else(|| "none".into());
+                    let e = &mut self.entities[entity_index];
+                    e.kind = MapEntityKind::Structure;
+                    e.type_id = building_type.to_string();
+                    e.speed = 0;
+                    e.target_x = None;
+                    e.target_y = None;
+                    e.path.clear();
+                    e.move_accum = 0;
+                    e.attack_target = None;
+                    e.attack_range = 0;
+                    e.attack_damage = 0;
+                    e.attack_cooldown = 0;
+                    e.attack_verses = full_verses();
+                    e.armor = armor;
+                    e.hva_frame = 0;
+                }
+                GameCommand::PlaceBuilding { player, ref type_id, x, y } => {
+                    let Some(player_index) = self.players.iter().position(|p| p.id == player)
+                    else {
+                        self.reject(command_index, CommandRejectReason::EntityNotFound);
+                        continue;
+                    };
+                    let house = self.players[player_index].house.clone();
+                    let Some(tt) = self.techno_types.get(type_id)
+                    else {
+                        self.reject(command_index, CommandRejectReason::InvalidPlacement);
+                        continue;
+                    };
+                    if tt.kind != TechnoKind::Building {
+                        self.reject(command_index, CommandRejectReason::InvalidPlacement);
+                        continue;
+                    }
+                    if is_construction_yard(type_id) {
+                        self.reject(command_index, CommandRejectReason::InvalidPlacement);
+                        continue;
+                    }
+                    if !self.house_has_living_yard(&house) {
+                        self.reject(command_index, CommandRejectReason::MissingPrerequisite);
+                        continue;
+                    }
+                    if requires_power_plant(type_id) && !self.house_has_living_power(&house) {
+                        self.reject(command_index, CommandRejectReason::MissingPrerequisite);
+                        continue;
+                    }
+                    if !self.can_place_structure(x, y) {
+                        self.reject(command_index, CommandRejectReason::InvalidPlacement);
+                        continue;
+                    }
+                    let cost = tt.cost as i32;
+                    if self.players[player_index].funds < cost {
+                        self.reject(command_index, CommandRejectReason::InsufficientFunds);
+                        continue;
+                    }
+                    let power = building_power_delta(type_id);
+                    let max_health = tt.strength.max(1);
+                    let armor = tt.armor.clone();
+                    let id = self.alloc_entity_id();
+                    self.players[player_index].funds -= cost;
+                    self.players[player_index].funds_spent = self.players[player_index].funds_spent.saturating_add(cost);
+                    if power >= 0 {
+                        self.players[player_index].power_output = self.players[player_index].power_output.saturating_add(power);
+                    }
+                    else {
+                        self.players[player_index].power_drain = self.players[player_index].power_drain.saturating_add(-power);
+                    }
+                    self.pass_grid.set_passable(x, y, false);
+                    self.entities.push(WorldEntity {
+                        id,
+                        kind: MapEntityKind::Structure,
+                        owner: house,
+                        type_id: type_id.to_ascii_uppercase(),
+                        x,
+                        y,
+                        facing: 0,
+                        turret_facing: 0,
+                        sub_cell: 0,
+                        health: max_health,
+                        max_health,
+                        speed: 0,
+                        armor,
+                        attack_range: 0,
+                        attack_damage: 0,
+                        attack_cooldown_max: 0,
+                        attack_verses: full_verses(),
+                        techno_kind: Some(TechnoKind::Building),
+                        target_x: None,
+                        target_y: None,
+                        path: Vec::new(),
+                        move_accum: 0,
+                        hva_frame: 0,
+                        attack_target: None,
+                        attack_cooldown: 0,
+                        ore_trip_accum: 0,
+                        produce_queue: None,
+                        rally_x: None,
+                        rally_y: None,
+                        hit_flash: 0,
+                        dead: false,
+                    });
+                }
+                GameCommand::Produce { player, ref type_id } => {
+                    let Some(player_index) = self.players.iter().position(|p| p.id == player)
+                    else {
+                        self.reject(command_index, CommandRejectReason::EntityNotFound);
+                        continue;
+                    };
+                    let house = self.players[player_index].house.clone();
+                    let Some(tt) = self.techno_types.get(type_id)
+                    else {
+                        self.reject(command_index, CommandRejectReason::InvalidPlacement);
+                        continue;
+                    };
+                    if !matches!(tt.kind, TechnoKind::Infantry | TechnoKind::Vehicle) {
+                        self.reject(command_index, CommandRejectReason::InvalidPlacement);
+                        continue;
+                    }
+                    let Some(factory_index) = self.find_idle_factory(&house, tt.kind)
+                    else {
+                        let has_busy = self.find_factory(&house, tt.kind).is_some();
+                        if has_busy {
+                            self.reject(command_index, CommandRejectReason::QueueFull);
+                        }
+                        else {
+                            self.reject(command_index, CommandRejectReason::MissingPrerequisite);
+                        }
+                        continue;
+                    };
+                    let cost = tt.cost as i32;
+                    if self.players[player_index].funds < cost {
+                        self.reject(command_index, CommandRejectReason::InsufficientFunds);
+                        continue;
+                    }
+                    self.players[player_index].funds -= cost;
+                    self.players[player_index].funds_spent = self.players[player_index].funds_spent.saturating_add(cost);
+                    self.entities[factory_index].produce_queue = Some((type_id.to_ascii_uppercase(), PRODUCE_TICKS));
+                }
+                GameCommand::SetRallyPoint { factory_index, x, y } => {
+                    if factory_index >= self.entities.len() {
+                        self.reject(command_index, CommandRejectReason::EntityNotFound);
+                        continue;
+                    }
+                    if self.entities[factory_index].dead {
+                        self.reject(command_index, CommandRejectReason::EntityDead);
+                        continue;
+                    }
+                    if !is_production_factory(&self.entities[factory_index].type_id) {
+                        self.reject(command_index, CommandRejectReason::InvalidTarget);
+                        continue;
+                    }
+                    if !self.pass_grid.in_bounds(x, y) {
+                        self.reject(command_index, CommandRejectReason::InvalidPlacement);
+                        continue;
+                    }
+                    let e = &mut self.entities[factory_index];
+                    e.rally_x = Some(x);
+                    e.rally_y = Some(y);
+                }
+            }
+        }
+    }
+
+    pub(crate) fn reject(&mut self, command_index: usize, reason: crate::CommandRejectReason) {
+        self.last_rejects.push(crate::CommandReject { command_index, reason });
+    }
+}
