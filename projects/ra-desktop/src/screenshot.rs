568861ede901db93eb66716848f4255c8a7e4031
aster <aster@vers.site>
2013-12-31 21:28:43 +0000

    ✨ Dump key UI screenshots from automated tests
    Add `dump_all_key_screens` so cargo test writes stable acceptance PNGs for menu lobby match and results without a GPU window.

--- projects/ra-desktop/src/screenshot.rs
+++ projects/ra-desktop/src/screenshot.rs
@@ -1,25 +1,39 @@
-//! 关键页验收截图：GPU 回读后落盘 PNG（默认不进 git）。
+//! 关键页验收截图：GPU 回读 / CPU 布局导出 PNG（默认不进 git）。
 //!
-//! - `F12`：截当前页
+//! - `F12`：截当前页（GPU 回读）
 //! - `RA2_AUTO_SCREENSHOT=1`：进入关键页时各截一次
-//! - `RA2_SCREENSHOT_DIR`：输出目录（默认 `./screenshots`）
+//! - `RA2_SCREENSHOT_DIR`：输出根目录（默认 `./screenshots`）
+//! - `cargo test -p ra-desktop dump_key_ui_screenshots`：自动导出关键页占位图
 
 use std::{
     collections::HashSet,
     path::{Path, PathBuf},
+    sync::Arc,
     time::{SystemTime, UNIX_EPOCH},
 };
 
-use ra_renderer::{RgbaImage, write_png_file};
-use ra_types::RaResult;
+use ra_engine::{HudSnapshot, MatchOutcome, SnapshotPlayer, SnapshotProduceQueue};
+use ra_map::Theater;
+use ra_renderer::{RgbaImage, ScreenChromeQuad, write_png_file};
+use ra_types::{EntityId, RaResult};
 
-use crate::screen::OriginalScreen;
+use crate::{
+    boot::BootMapCandidate,
+    hud_chrome::{self, ResultsHit},
+    menu_view::{layout_for, layout_skirmish_lobby},
+    screen::OriginalScreen,
+};
 
-/// 截图输出目录。
+/// 截图输出根目录。
 pub fn screenshot_dir() -> PathBuf {
     std::env::var_os("RA2_SCREENSHOT_DIR")
         .map(PathBuf::from)
         .unwrap_or_else(|| PathBuf::from("screenshots"))
+}
+
+/// 自动测试验收图目录（稳定文件名，便于打开对照）。
+pub fn acceptance_dir() -> PathBuf {
+    screenshot_dir().join("acceptance")
 }
 
 /// 是否在进入关键页时自动截图。
@@ -76,24 +90,190 @@ pub fn save_screenshot(screen: &str, image: &RgbaImage) -> RaResult<PathBuf> {
     save_screenshot_to(screenshot_dir(), screen, image)
 }
 
-/// 指定目录落盘。
+/// 指定目录落盘（带时间戳）。
 pub fn save_screenshot_to(dir: impl AsRef<Path>, screen: &str, image: &RgbaImage) -> RaResult<PathBuf> {
     let ms = SystemTime::now()
         .duration_since(UNIX_EPOCH)
         .map(|d| d.as_millis())
         .unwrap_or(0);
-    let safe: String = screen
-        .chars()
-        .map(|c| if c.is_ascii_alphanumeric() || c == '_' || c == '-' { c } else { '_' })
-        .collect();
+    let safe = sanitize_name(screen);
     let path = dir.as_ref().join(format!("{safe}_{ms}.png"));
     write_png_file(&path, image)?;
     Ok(path)
 }
 
+/// 验收用稳定文件名：`{dir}/{screen}.png`（覆盖写）。
+pub fn save_acceptance_png(dir: impl AsRef<Path>, screen: &str, image: &RgbaImage) -> RaResult<PathBuf> {
+    let path = dir.as_ref().join(format!("{}.png", sanitize_name(screen)));
+    write_png_file(&path, image)?;
+    Ok(path)
+}
+
+fn sanitize_name(screen: &str) -> String {
+    screen
+        .chars()
+        .map(|c| {
+            if c.is_ascii_alphanumeric() || c == '_' || c == '-' {
+                c
+            }
+            else {
+                '_'
+            }
+        })
+        .collect()
+}
+
 /// 仅用于单元测试的路径拼接（不写盘）。
 pub fn planned_path(dir: &Path, screen: &str, ms: u128) -> PathBuf {
     dir.join(format!("{screen}_{ms}.png"))
+}
+
+/// 把归一化色块叠到 RGBA 底图上（CPU，供无 GPU 的验收导出）。
+pub fn paint_screen_chrome(dst: &mut RgbaImage, quads: &[ScreenChromeQuad]) {
+    let w = dst.width.max(1) as f32;
+    let h = dst.height.max(1) as f32;
+    for q in quads {
+        let x0 = (q.x0.clamp(0.0, 1.0) * w) as u32;
+        let y0 = (q.y0.clamp(0.0, 1.0) * h) as u32;
+        let x1 = (q.x1.clamp(0.0, 1.0) * w) as u32;
+        let y1 = (q.y1.clamp(0.0, 1.0) * h) as u32;
+        let src = [
+            (q.color[0].clamp(0.0, 1.0) * 255.0) as u8,
+            (q.color[1].clamp(0.0, 1.0) * 255.0) as u8,
+            (q.color[2].clamp(0.0, 1.0) * 255.0) as u8,
+            (q.color[3].clamp(0.0, 1.0) * 255.0) as u8,
+        ];
+        blend_rect(dst, x0, y0, x1.saturating_sub(x0).max(1), y1.saturating_sub(y0).max(1), src);
+    }
+}
+
+fn blend_rect(dst: &mut RgbaImage, x: u32, y: u32, bw: u32, bh: u32, src: [u8; 4]) {
+    let x1 = (x + bw).min(dst.width);
+    let y1 = (y + bh).min(dst.height);
+    let sa = src[3] as u32;
+    if sa == 0 {
+        return;
+    }
+    for py in y..y1 {
+        for px in x..x1 {
+            let i = ((py * dst.width + px) * 4) as usize;
+            if sa >= 255 {
+                dst.pixels[i..i + 4].copy_from_slice(&src);
+                continue;
+            }
+            let inv = 255 - sa;
+            for c in 0..3 {
+                let d = dst.pixels[i + c] as u32;
+                dst.pixels[i + c] = ((src[c] as u32 * sa + d * inv) / 255) as u8;
+            }
+            dst.pixels[i + 3] = 255;
+        }
+    }
+}
+
+fn solid_bg(width: u32, height: u32, rgba: [u8; 4]) -> RgbaImage {
+    let mut pixels = vec![0u8; (width as usize) * (height as usize) * 4];
+    for px in pixels.chunks_exact_mut(4) {
+        px.copy_from_slice(&rgba);
+    }
+    RgbaImage::new(width, height, pixels).expect("solid bg")
+}
+
+fn sample_hud() -> HudSnapshot {
+    HudSnapshot {
+        tick: 240,
+        players: vec![SnapshotPlayer {
+            house: Arc::from("Americans"),
+            funds: 4200,
+            power_output: 100,
+            power_drain: 60,
+            low_power: false,
+        }],
+        produce_queues: vec![SnapshotProduceQueue {
+            factory: EntityId(7),
+            type_id: Arc::from("E1"),
+            remaining_ticks: 8,
+            rally_x: None,
+            rally_y: None,
+        }],
+        last_rejects: vec![],
+        outcome: None,
+        paused: false,
+        pause_reason: None,
+        match_stats: None,
+    }
+}
+
+/// 导出全部关键占位页到 `screenshots/acceptance/*.png`（覆盖写）。
+///
+/// **无窗口 / 无 GPU**：菜单走 `menu_view` 色块布局；对局/结算把 HUD chrome 叠在合成底图上。
+/// 这是验收对照图，不是原版 SHP 截图。
+pub fn dump_all_key_screens() -> RaResult<Vec<PathBuf>> {
+    dump_all_key_screens_to(acceptance_dir())
+}
+
+/// 导出到指定目录。
+pub fn dump_all_key_screens_to(dir: impl AsRef<Path>) -> RaResult<Vec<PathBuf>> {
+    let dir = dir.as_ref();
+    let (w, h) = (1280u32, 720u32);
+    let mut out = Vec::new();
+
+    let menu_pages = [
+        OriginalScreen::MainMenu,
+        OriginalScreen::SinglePlayerMenu,
+        OriginalScreen::LoadScreen,
+        OriginalScreen::Options,
+        OriginalScreen::Network,
+    ];
+    for screen in menu_pages {
+        let layout = layout_for(screen, w, h, None, None).expect("menu layout");
+        out.push(save_acceptance_png(dir, screen.as_str(), &layout.image)?);
+    }
+
+    let maps = [
+        BootMapCandidate {
+            file_name: "mp03t4.map".into(),
+            width: 100,
+            height: 90,
+            theater: Theater::Temperate,
+        },
+        BootMapCandidate {
+            file_name: "sample_snow.map".into(),
+            width: 80,
+            height: 80,
+            theater: Theater::Snow,
+        },
+    ];
+    let lobby = layout_skirmish_lobby(
+        w,
+        h,
+        &maps,
+        Some("mp03t4.map"),
+        "allies",
+        "normal",
+        Some(0),
+        None,
+    );
+    out.push(save_acceptance_png(dir, OriginalScreen::SkirmishLobby.as_str(), &lobby.image)?);
+
+    let mut match_img = solid_bg(w, h, [24, 48, 28, 255]);
+    let hud = sample_hud();
+    paint_screen_chrome(&mut match_img, &hud_chrome::match_hud_chrome(&hud, Some("Americans")));
+    out.push(save_acceptance_png(dir, OriginalScreen::Match.as_str(), &match_img)?);
+
+    let mut results_img = solid_bg(w, h, [20, 28, 40, 255]);
+    let mut results_hud = sample_hud();
+    results_hud.outcome = Some(MatchOutcome::Victory {
+        owner: "Americans".into(),
+    });
+    paint_screen_chrome(
+        &mut results_img,
+        &hud_chrome::match_hud_chrome(&results_hud, Some("Americans")),
+    );
+    paint_screen_chrome(&mut results_img, &hud_chrome::results_chrome(Some(ResultsHit::Rematch)));
+    out.push(save_acceptance_png(dir, OriginalScreen::Results.as_str(), &results_img)?);
+
+    Ok(out)
 }
 
 #[cfg(test)]
@@ -118,5 +298,37 @@ mod tests {
         assert!(t.should_capture_if(OriginalScreen::MainMenu, true));
         assert!(!t.should_capture_if(OriginalScreen::MainMenu, true));
         assert!(!t.should_capture_if(OriginalScreen::MainMenu, false));
+    }
+
+    #[test]
+    fn dump_key_ui_screenshots_for_acceptance() {
+        let paths = dump_all_key_screens().expect("dump key screens");
+        let dir = acceptance_dir();
+        assert!(paths.len() >= 7, "expected key screens, got {}", paths.len());
+        for path in &paths {
+            assert!(path.exists(), "missing {}", path.display());
+            let len = std::fs::metadata(path).unwrap().len();
+            assert!(len > 200, "{} too small ({len})", path.display());
+        }
+        assert!(dir.join("main_menu.png").exists());
+        assert!(dir.join("skirmish_lobby.png").exists());
+        assert!(dir.join("match.png").exists());
+        assert!(dir.join("results.png").exists());
+    }
+
+    #[test]
+    fn paint_chrome_covers_pixels() {
+        let mut img = solid_bg(100, 100, [0, 0, 0, 255]);
+        paint_screen_chrome(
+            &mut img,
+            &[ScreenChromeQuad {
+                x0: 0.0,
+                y0: 0.0,
+                x1: 0.5,
+                y1: 0.5,
+                color: [1.0, 0.0, 0.0, 1.0],
+            }],
+        );
+        assert_eq!(img.pixels[0..3], [255, 0, 0]);
     }
 }
