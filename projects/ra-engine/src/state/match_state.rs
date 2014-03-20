7826ceb1298b8b7a096b5f5046cd5ef01727b228
aster <aster@vers.site>
2014-03-20 00:43:38 +0000

    🐛 Give unbound techno units zero combat stats
    Without a matching techno definition, inventing default range damage and verses created fightable ghost units.
    Keep unbound entities non-combat until rules bind succeeds.

--- projects/ra-engine/src/state/match_state.rs
+++ projects/ra-engine/src/state/match_state.rs
@@ -10,7 +10,7 @@ use ra_types::{CommandId, EntityId, GameEdition, PlayerId, RuntimeDefinitions, S
 
 use super::{entities::WorldEntity, players::PlayerState};
 use crate::{
-    gameplay::{full_verses, verses_for},
+    gameplay::verses_for,
     game::{CommandReject, GameCommand, InputFrame},
     presentation::DirtyEntitySet,
     spatial::{is_mobile, repath_at},
@@ -97,14 +97,14 @@ impl MatchState {
                 let max_health = tt.map(|t| t.strength).unwrap_or(1).max(1);
                 let health = (u64::from(max_health) * u64::from(e.health) / 256) as u32;
                 let speed = tt.map(|t| t.speed).unwrap_or(0);
-                let attack_range =
-                    tt.map(|t| if t.range > 0 { t.range } else { t.sight.max(1) }).unwrap_or(DEFAULT_ATTACK_RANGE);
+                // 无 techno 定义时禁止发明默认射程/伤害（否则会变成可战斗幽灵单位）。
+                let attack_range = tt.map(|t| if t.range > 0 { t.range } else { t.sight.max(1) }).unwrap_or(0);
                 let attack_damage =
-                    tt.map(|t| if t.damage > 0 { t.damage } else { (t.strength / 4).max(1) }).unwrap_or(DEFAULT_ATTACK_DAMAGE);
+                    tt.map(|t| if t.damage > 0 { t.damage } else { (t.strength / 4).max(1) }).unwrap_or(0);
                 let attack_cooldown_max =
-                    tt.map(|t| if t.rof > 0 { t.rof } else { ATTACK_COOLDOWN_TICKS }).unwrap_or(ATTACK_COOLDOWN_TICKS);
+                    tt.map(|t| if t.rof > 0 { t.rof } else { ATTACK_COOLDOWN_TICKS }).unwrap_or(0);
                 let armor = tt.map(|t| t.armor.clone()).unwrap_or_else(|| "none".into());
-                let attack_verses = tt.map(|t| verses_for(&definitions, &t.warhead)).unwrap_or_else(full_verses);
+                let attack_verses = tt.map(|t| verses_for(&definitions, &t.warhead)).unwrap_or([0; 11]);
                 let techno_kind = tt.map(|t| techno_class_to_kind(t.class));
                 let id = EntityId(next_entity_id);
                 next_entity_id = next_entity_id.saturating_add(1);
--- projects/ra-engine/tests/engine/state/bind.rs
+++ projects/ra-engine/tests/engine/state/bind.rs
@@ -32,3 +32,27 @@ fn binds_strength_and_speed() {
     assert_eq!(e.techno_kind, Some(TechnoKind::Vehicle));
     assert_eq!(world.bound_techno_count(), 1);
 }
+
+#[test]
+fn unbound_techno_gets_zero_combat_stats() {
+    let rules = rules_with_mtnk();
+    let mut map = map_with_size();
+    map.entities.push(MapEntity {
+        kind: MapEntityKind::Unit,
+        owner: "Americans".into(),
+        type_id: "NOSUCH".into(),
+        health: 256,
+        x: 10,
+        y: 20,
+        facing: 0,
+        sub_cell: 0,
+    });
+    let world = MatchState::new(GameEdition::Ra2, &rules, map);
+    let e = &world.entities[0];
+    assert_eq!(e.attack_range, 0);
+    assert_eq!(e.attack_damage, 0);
+    assert_eq!(e.attack_cooldown_max, 0);
+    assert_eq!(e.attack_verses, [0; 11]);
+    assert!(e.techno_kind.is_none());
+    assert_eq!(world.bound_techno_count(), 0);
+}
