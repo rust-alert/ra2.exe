124f924e9e8c34f1e82bc378b613d8c1e7e60e50
aster <aster@vers.site>
2014-03-27 09:26:40 +0000

    🐛 Reject unpowered structure placement as `InsufficientPower`
    Power-gated place checks were reporting `MissingPrerequisite`.
    Use the dedicated power reject so HUD copy matches the real failure.

--- projects/ra-engine/tests/engine/presentation/hud/reject.rs
+++ projects/ra-engine/tests/engine/presentation/hud/reject.rs
@@ -5,6 +5,7 @@ use ra_engine::CommandRejectReason;
 #[test]
 fn hud_labels_cover_economy_rejects() {
     assert_eq!(CommandRejectReason::InsufficientFunds.as_hud_label(), "资金不足");
+    assert_eq!(CommandRejectReason::InsufficientPower.as_hud_label(), "电力不足");
     assert_eq!(CommandRejectReason::MissingPrerequisite.as_hud_label(), "前置不足");
     assert_eq!(CommandRejectReason::QueueFull.as_hud_label(), "队列已满");
 }
