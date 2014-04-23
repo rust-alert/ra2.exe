2c7ad20b5cd6bdda9d34fddecb878a5d5b2ed4ad
aster <aster@vers.site>
2014-04-23 16:45:47 +0000

    ♻️ Rename GUI binary to `rust-ra2`
    Avoid colliding with retail `ra2.exe` when the engine binary is dropped into an install directory.

--- projects/ra-desktop/src/main.rs
+++ projects/ra-desktop/src/main.rs
@@ -1,7 +1,8 @@
-//! 原生 GUI 入口：二进制名 `ra2`（Windows 上为 `ra2.exe`）。
+//! 原生 GUI 入口：二进制名 `rust-ra2`（Windows 上为 `rust-ra2.exe`）。
 //!
-//! 不是命令行工具——启动配置来自可执行文件同目录的 `RustAlert.toml`（可选），然后由窗口接管进程。
-//! 默认 `ra2_dir` 为 exe 所在目录，便于把二进制直接放进游戏安装目录启动。
+//! 不是命令行工具——启动配置来自可执行文件同目录的 `RustAlert.toml`，然后由窗口接管进程。
+//! 默认情形：未写 `ra2_dir` 时取 exe 所在目录，便于把二进制放进游戏安装目录定位资源；
+//! 分辨率等其它选项仍写在该配置文件中。
 //!
 //! 页面状态机见 [`shell::AppShell`] / [`screen::OriginalScreen`]；对局输入见 [`match_ctrl::MatchController`]。
 //! 产品路径对齐原版主 UI 流程，禁止启动后自动开局。
@@ -20,6 +21,8 @@ mod screen;
 mod screenshot;
 mod shell;
 mod skirmish_setup;
+#[cfg(feature = "test-harness")]
+mod test_boot;
 mod ui_assets;
 mod ui_compose;
 mod ui_decode;
@@ -28,8 +31,6 @@ mod ui_layout;
 mod ui_page;
 mod ui_resolve;
 mod ui_slots;
-#[cfg(feature = "test-harness")]
-mod test_boot;
 
 use ra_types::RaResult;
 use tracing_appender::non_blocking::WorkerGuard;
@@ -37,7 +38,7 @@ use tracing_subscriber::{EnvFilter, fmt, layer::SubscriberExt, util::SubscriberI
 
 fn init_tracing() -> WorkerGuard {
     let _ = std::fs::create_dir_all("logs");
-    let file_appender = tracing_appender::rolling::never("logs", "ra2.log");
+    let file_appender = tracing_appender::rolling::never("logs", "rust-ra2.log");
     let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
     let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
     let stderr_layer = fmt::layer().with_writer(std::io::stderr);
@@ -48,7 +49,7 @@ fn init_tracing() -> WorkerGuard {
 
 fn main() {
     if let Err(e) = run() {
-        eprintln!("ra2 错误: {e}");
+        eprintln!("rust-ra2 错误: {e}");
         tracing::error!("致命错误: {e}");
         std::process::exit(1);
     }
@@ -56,6 +57,6 @@ fn main() {
 
 fn run() -> RaResult<()> {
     let _log_guard = init_tracing();
-    tracing::info!("ra2 启动");
+    tracing::info!("rust-ra2 启动");
     shell::run_shell()
 }
