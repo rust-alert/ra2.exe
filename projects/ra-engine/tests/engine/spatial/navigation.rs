b304af83920638a73881ee3753f391a6a4d466c4
aster <aster@vers.site>
2014-05-05 14:06:49 +0000

    🐛 Prefer local house in `config.toml.example` detour test and drop `config.toml.example`
    The Neutral wall seeded the local house first so `MoveTo` was rejected as `WrongOwner`. Prefer `Americans` before ordering the tank. Drop the obsolete `config.toml.example` now that `RustAlert.toml.example` is the template.

--- projects/ra-engine/tests/engine/spatial/navigation.rs
+++ projects/ra-engine/tests/engine/spatial/navigation.rs
@@ -31,6 +31,8 @@ fn bfs_detours_around_structure() {
         sub_cell: 0,
     });
     let mut world = MatchState::new(GameEdition::Ra2, &rules, map);
+    // 墙体属 Neutral，先入房主序会把本地玩家落在 Neutral；命令需切到美国人。
+    assert!(world.prefer_local_house("Americans"));
     assert!(!world.pass_grid.is_passable(12, 10));
     world.push_command(GameCommand::MoveTo { entity: EntityId(2), x: 14, y: 10 });
     world.advance_tick();
