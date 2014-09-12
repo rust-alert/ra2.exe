edd004715b736b03142020b9c80b6cf9af8ded39
aster <aster@vers.site>
2014-09-12 08:38:29 +0000

    🔧 Sort host package type exports for Biome
    Reorder `ExtractedFile` ahead of `ExtractOptions` so `pnpm run lint` passes.

--- projects/hosts/red-alert2/src/index.ts
+++ projects/hosts/red-alert2/src/index.ts
@@ -1,8 +1,8 @@
 /** Node / CLI 默认面：只导出 native。Wasm 请走 `@game-gpt/red-alert2/wasm`。 */
 export type {
+    ExtractedFile,
     ExtractOptions,
     ExtractResult,
-    ExtractedFile,
     LaunchOptions,
     NativeBinaryIdentity,
     NativeBinding,
