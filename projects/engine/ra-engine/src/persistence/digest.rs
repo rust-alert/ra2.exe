11d945c379c37126ff8fa2c713049906d0220c91
aster <aster@vers.site>
2014-12-27 06:05:08 +0000

    ✨ Hash lockstep digest from ECS components
    ehash reads Transform, Health, `CombatStats`, and related components instead of `WorldEntity` projection fields while preserving the same mix order.

--- projects/engine/ra-engine/src/persistence/digest.rs
+++ projects/engine/ra-engine/src/persistence/digest.rs
@@ -1,6 +1,15 @@
 //! 确定性状态摘要（锁步校验用）。
 
-use crate::{game::GameCommand, state::MatchState};
+use crate::{
+    game::GameCommand,
+    state::{
+        MatchState,
+        components::{
+            AnimationState, AttackState, CombatStats, HarvesterState, Health, Identity, Locomotor, Owner,
+            ProductionQueue, Transform,
+        },
+    },
+};
 use ra_types::ScheduledCommand;
 
 impl MatchState {
@@ -12,33 +21,65 @@ impl MatchState {
         for cmd in &self.last_input_frame.commands {
             h = hash_scheduled(h, cmd);
         }
-        for e in &self.entities {
+        let ids: Vec<_> = self.entities.iter().map(|e| e.id).collect();
+        for id in ids {
+            let xf = self.ecs_get::<Transform>(id).copied();
+            let health = self.ecs_get::<Health>(id).copied();
+            let loco = self.ecs_get::<Locomotor>(id).copied();
+            let attack = self.ecs_get::<AttackState>(id).copied();
+            let anim = self.ecs_get::<AnimationState>(id).copied();
+            let harvester = self.ecs_get::<HarvesterState>(id).copied();
+            let armor = self.ecs_get::<CombatStats>(id).map(|s| s.armor.clone()).unwrap_or_default();
+            let attack_verses = self.ecs_get::<CombatStats>(id).map(|s| s.attack_verses).unwrap_or([0; 11]);
+            let attack_range = self.ecs_get::<CombatStats>(id).map(|s| s.attack_range).unwrap_or(0);
+            let attack_damage = self.ecs_get::<CombatStats>(id).map(|s| s.attack_damage).unwrap_or(0);
+            let attack_cooldown_max = self.ecs_get::<CombatStats>(id).map(|s| s.attack_cooldown_max).unwrap_or(0);
+            let produce_item = self.ecs_get::<ProductionQueue>(id).and_then(|p| p.item.clone());
+            let rally_x = self.ecs_get::<ProductionQueue>(id).and_then(|p| p.rally_x);
+            let rally_y = self.ecs_get::<ProductionQueue>(id).and_then(|p| p.rally_y);
+            let type_id = self.ecs_get::<Identity>(id).map(|i| i.type_id.clone());
+            let owner = self.ecs_get::<Owner>(id).map(|o| o.house.clone());
+
+            let x = xf.map(|t| t.x).unwrap_or(0);
+            let y = xf.map(|t| t.y).unwrap_or(0);
+            let facing = xf.map(|t| t.facing).unwrap_or(0);
+            let turret_facing = xf.map(|t| t.turret_facing).unwrap_or(0);
+            let current_health = health.map(|h| h.current).unwrap_or(0);
+            let max_health = health.map(|h| h.maximum).unwrap_or(0);
+            let dead = health.map(|h| h.dead).unwrap_or(false);
+            let speed = loco.map(|l| l.speed).unwrap_or(0);
+            let hva_frame = anim.map(|a| a.hva_frame).unwrap_or(0);
+            let hit_flash = anim.map(|a| a.hit_flash).unwrap_or(0);
+            let attack_cooldown = attack.map(|a| a.cooldown).unwrap_or(0);
+            let attack_target = attack.and_then(|a| a.target);
+            let ore_trip_accum = harvester.map(|h| h.ore_trip_accum).unwrap_or(0);
+
             h = h
                 .wrapping_mul(1099511628211)
-                .wrapping_add(e.id.0)
-                .wrapping_add(e.x as u64)
-                .wrapping_add((e.y as u64) << 16)
-                .wrapping_add((e.facing as u64) << 32)
-                .wrapping_add((e.turret_facing as u64) << 40)
-                .wrapping_add(u64::from(e.health))
-                .wrapping_add(u64::from(e.max_health).wrapping_shl(1))
-                .wrapping_add(u64::from(e.speed).wrapping_shl(2))
-                .wrapping_add(u64::from(e.attack_range).wrapping_shl(3))
-                .wrapping_add(u64::from(e.attack_damage).wrapping_shl(4))
-                .wrapping_add(u64::from(e.attack_cooldown_max).wrapping_shl(5))
-                .wrapping_add(u64::from(e.dead))
-                .wrapping_add(u64::from(e.hva_frame) << 8)
-                .wrapping_add(u64::from(e.attack_cooldown) << 24)
-                .wrapping_add(u64::from(e.ore_trip_accum) << 8)
-                .wrapping_add(u64::from(e.hit_flash) << 16)
-                .wrapping_add(e.attack_target.map(|id| id.0).unwrap_or(0) << 32);
-            for b in e.armor.as_bytes() {
+                .wrapping_add(id.0)
+                .wrapping_add(x as u64)
+                .wrapping_add((y as u64) << 16)
+                .wrapping_add((facing as u64) << 32)
+                .wrapping_add((turret_facing as u64) << 40)
+                .wrapping_add(u64::from(current_health))
+                .wrapping_add(u64::from(max_health).wrapping_shl(1))
+                .wrapping_add(u64::from(speed).wrapping_shl(2))
+                .wrapping_add(u64::from(attack_range).wrapping_shl(3))
+                .wrapping_add(u64::from(attack_damage).wrapping_shl(4))
+                .wrapping_add(u64::from(attack_cooldown_max).wrapping_shl(5))
+                .wrapping_add(u64::from(dead))
+                .wrapping_add(u64::from(hva_frame) << 8)
+                .wrapping_add(u64::from(attack_cooldown) << 24)
+                .wrapping_add(u64::from(ore_trip_accum) << 8)
+                .wrapping_add(u64::from(hit_flash) << 16)
+                .wrapping_add(attack_target.map(|tid| tid.0).unwrap_or(0) << 32);
+            for b in armor.as_bytes() {
                 h = h.wrapping_mul(1099511628211).wrapping_add(u64::from(*b));
             }
-            for v in e.attack_verses {
+            for v in attack_verses {
                 h = h.wrapping_mul(1099511628211).wrapping_add(u64::from(v));
             }
-            if let Some((ref qid, rem)) = e.produce_queue {
+            if let Some((ref qid, rem)) = produce_item {
                 h = h.wrapping_mul(1099511628211).wrapping_add(u64::from(rem));
                 for b in qid.as_bytes() {
                     h = h.wrapping_mul(1099511628211).wrapping_add(u64::from(*b));
@@ -46,13 +87,17 @@ impl MatchState {
             }
             h = h
                 .wrapping_mul(1099511628211)
-                .wrapping_add(e.rally_x.map(u64::from).unwrap_or(0))
-                .wrapping_add(e.rally_y.map(|v| u64::from(v) << 16).unwrap_or(0));
-            for b in e.type_id.as_bytes() {
-                h = h.wrapping_mul(1099511628211).wrapping_add(u64::from(*b));
+                .wrapping_add(rally_x.map(u64::from).unwrap_or(0))
+                .wrapping_add(rally_y.map(|v| u64::from(v) << 16).unwrap_or(0));
+            if let Some(type_id) = type_id {
+                for b in type_id.as_bytes() {
+                    h = h.wrapping_mul(1099511628211).wrapping_add(u64::from(*b));
+                }
             }
-            for b in e.owner.as_bytes() {
-                h = h.wrapping_mul(1099511628211).wrapping_add(u64::from(*b));
+            if let Some(owner) = owner {
+                for b in owner.as_bytes() {
+                    h = h.wrapping_mul(1099511628211).wrapping_add(u64::from(*b));
+                }
             }
         }
         for p in &self.players {
