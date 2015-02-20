ad594995d69e8ad2f10b1049300631893f5967e2
aster <aster@vers.site>
2015-02-20 06:36:37 +0000

    ✨ Wire dialog `0x102` through `LayoutEngine` snapshot
    Resolve skirmish lobby controls with the same `tile_snap` and anchor policies.
    Golden rects and hit targets now share one `LayoutSnapshot`.

--- projects/engine/ra-layout/src/reference/mod.rs
+++ projects/engine/ra-layout/src/reference/mod.rs
@@ -1,9 +1,11 @@
 //! 模板与遗留参照。
 
+mod dialog_0x102;
 mod dialog_0x6b;
 mod dlu;
 mod legacy;
 
+pub use dialog_0x102::{dialog_0x102_layout_tree, resolve_dialog_0x102};
 pub use dialog_0x6b::{dialog_0x6b_layout_tree, resolve_dialog_0x6b, shell_design_size};
 pub use dlu::{mul_div_round, DluRect, FontBaseUnits, MS_SANS_SERIF_8PT};
 pub use legacy::{LegacyReference, LegacyRole, LegacySource};
---
+++ projects/engine/ra-layout/tests/dialog_0x102.rs
@@ -0,0 +1,69 @@
+//! `RT_DIALOG` `0x102`：模板 → `LayoutEngine` → snapshot / hit。
+
+use ra_layout::{
+    dialog_0x102_layout_tree, LayoutEngine, Point2, Rect, RightPanelChrome, Viewport,
+};
+
+#[test]
+fn dialog_0x102_snapshot_matches_skirmish_golden_rects() {
+    let chrome = RightPanelChrome::shell_defaults();
+    let root = dialog_0x102_layout_tree(chrome);
+    let snap = LayoutEngine.solve(
+        Viewport {
+            size: ra_layout::shell_design_size(chrome),
+            ..Viewport::default()
+        },
+        &root,
+    );
+
+    assert_eq!(
+        snap.get("start").map(|e| e.layout.rect),
+        Some(Rect::from_xywh(644.0, 241.0, 156.0, 42.0))
+    );
+    assert_eq!(
+        snap.get("choose_map").map(|e| e.layout.rect),
+        Some(Rect::from_xywh(644.0, 283.0, 156.0, 42.0))
+    );
+    assert_eq!(
+        snap.get("back").map(|e| e.layout.rect),
+        Some(Rect::from_xywh(644.0, 535.0, 156.0, 42.0))
+    );
+    assert_eq!(
+        snap.get("map_preview").map(|e| e.layout.rect),
+        Some(Rect::from_xywh(644.0, 37.0, 144.0, 112.0))
+    );
+    assert_eq!(
+        snap.get("title").map(|e| e.layout.rect),
+        Some(Rect::from_xywh(635.0, 2.0, 162.0, 16.0))
+    );
+    assert_eq!(
+        snap.get("map_name_plate").map(|e| e.layout.rect),
+        Some(Rect::from_xywh(644.0, 157.0, 156.0, 84.0))
+    );
+    assert_eq!(
+        snap.get("player_name").map(|e| e.layout.rect),
+        Some(Rect::from_xywh(53.0, 18.0, 150.0, 20.0))
+    );
+    assert_eq!(
+        snap.get("checkbox_quick").map(|e| e.layout.rect),
+        Some(Rect::from_xywh(53.0, 236.0, 150.0, 16.0))
+    );
+    assert_eq!(
+        snap.get("track_speed").map(|e| e.layout.rect),
+        Some(Rect::from_xywh(321.0, 236.0, 128.0, 21.0))
+    );
+    assert_eq!(
+        snap.get("label_speed").map(|e| e.layout.rect),
+        Some(Rect::from_xywh(219.0, 236.0, 90.0, 16.0))
+    );
+
+    assert_eq!(
+        snap
+            .hit_test(Point2 {
+                x: 650.0,
+                y: 250.0
+            })
+            .map(|e| e.id.0.as_str()),
+        Some("start")
+    );
+}
