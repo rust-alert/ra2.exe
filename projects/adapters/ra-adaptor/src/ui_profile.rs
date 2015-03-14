bd7aa6f92742a97f545e6c34ced67c90127f153e
aster <aster@vers.site>
2015-03-14 20:46:58 +0000

    ✨ Annotate shell dialog templates with `ControlPlacement`
    Fill `0x6B` and `0x102` control tables with `tile_snap`, anchor, and cover policies.
    Include the skirmish map-name plate as a chrome-derived control.

--- projects/adapters/ra-adaptor/src/ui_profile.rs
+++ projects/adapters/ra-adaptor/src/ui_profile.rs
@@ -1,9 +1,11 @@
 //! 壳层 `RuntimeUiProfile` 工厂（骨架：先填对话框 DLU 表）。
 
-use ra_types::{DialogControlDesc, DialogTemplate, RuntimeUiProfile, UiCapabilities};
+use ra_types::{
+    ControlPlacement, DialogControlDesc, DialogTemplate, RuntimeUiProfile, UiCapabilities,
+};
 
-fn ctrl(id: &str, x: i32, y: i32, w: i32, h: i32) -> DialogControlDesc {
-    DialogControlDesc::preserve(id, x, y, w, h)
+fn ctrl(id: &str, x: i32, y: i32, w: i32, h: i32, placement: ControlPlacement) -> DialogControlDesc {
+    DialogControlDesc::with_placement(id, x, y, w, h, placement)
 }
 
 /// 选图对话框 `0x6B` 控件 DLU（与 `ra-layout` 金标一致）。
@@ -11,15 +13,15 @@ pub fn dialog_template_0x6b() -> DialogTemplate {
     DialogTemplate {
         dialog_id: 0x6B,
         controls: vec![
-            ctrl("use_map", 318, 122, 108, 23),
-            ctrl("create_random", 318, 149, 108, 23),
-            // 取消钮不用模板 y，运行时贴底盖；此处保留占位便于对照。
-            ctrl("cancel", 318, 269, 108, 23),
-            ctrl("title", 318, 1, 108, 10),
-            ctrl("map_preview", 324, 23, 96, 69),
-            ctrl("label_engagement", 23, 20, 257, 12),
-            ctrl("game_type_list", 20, 78, 130, 160),
-            ctrl("map_list", 168, 78, 130, 160),
+            ctrl("use_map", 318, 122, 108, 23, ControlPlacement::TileSnap),
+            ctrl("create_random", 318, 149, 108, 23, ControlPlacement::TileSnap),
+            // 取消钮不用模板 y，运行时贴底盖；DLU 仅作对照占位。
+            ctrl("cancel", 318, 269, 108, 23, ControlPlacement::BottomCoverButton),
+            ctrl("title", 318, 1, 108, 10, ControlPlacement::RightPanelAnchor),
+            ctrl("map_preview", 324, 23, 96, 69, ControlPlacement::RightPanelAnchor),
+            ctrl("label_engagement", 23, 20, 257, 12, ControlPlacement::PreserveDlu),
+            ctrl("game_type_list", 20, 78, 130, 160, ControlPlacement::PreserveDlu),
+            ctrl("map_list", 168, 78, 130, 160, ControlPlacement::PreserveDlu),
         ],
     }
 }
@@ -29,15 +31,16 @@ pub fn dialog_template_0x102() -> DialogTemplate {
     DialogTemplate {
         dialog_id: 0x102,
         controls: vec![
-            ctrl("start", 318, 149, 108, 23),
-            ctrl("choose_map", 318, 176, 108, 23),
-            ctrl("back", 318, 269, 108, 23),
-            ctrl("title", 318, 1, 108, 10),
-            ctrl("map_preview", 324, 23, 96, 69),
-            ctrl("player_name", 35, 11, 100, 12),
-            ctrl("checkbox_quick", 35, 145, 100, 10),
-            ctrl("track_speed", 214, 145, 85, 13),
-            ctrl("label_speed", 146, 145, 60, 10),
+            ctrl("start", 318, 149, 108, 23, ControlPlacement::TileSnap),
+            ctrl("choose_map", 318, 176, 108, 23, ControlPlacement::TileSnap),
+            ctrl("back", 318, 269, 108, 23, ControlPlacement::BottomCoverButton),
+            ctrl("title", 318, 1, 108, 10, ControlPlacement::RightPanelAnchor),
+            ctrl("map_preview", 324, 23, 96, 69, ControlPlacement::RightPanelAnchor),
+            ctrl("map_name_plate", 0, 0, 0, 0, ControlPlacement::MapNamePlate),
+            ctrl("player_name", 35, 11, 100, 12, ControlPlacement::PreserveDlu),
+            ctrl("checkbox_quick", 35, 145, 100, 10, ControlPlacement::PreserveDlu),
+            ctrl("track_speed", 214, 145, 85, 13, ControlPlacement::PreserveDlu),
+            ctrl("label_speed", 146, 145, 60, 10, ControlPlacement::PreserveDlu),
         ],
     }
 }
@@ -65,5 +68,11 @@ mod tests {
         assert!(profile.dialog(0x102).is_some());
         assert!(profile.ui_capabilities.skirmish_lobby);
         assert!(!profile.ui_capabilities.create_random_map);
+        let choose = profile.dialog(0x6B).unwrap();
+        assert_eq!(choose.controls[0].placement, ControlPlacement::TileSnap);
+        assert_eq!(
+            choose.controls.iter().find(|c| c.id.0 == "cancel").map(|c| c.placement),
+            Some(ControlPlacement::BottomCoverButton)
+        );
     }
 }
