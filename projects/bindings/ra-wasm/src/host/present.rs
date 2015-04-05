a5495576611dd826db90d8344ab5e815e36d5f31
aster <aster@vers.site>
2015-04-05 01:54:33 +0000

    🐛 Gate wasm present surface APIs to wasm32
    Keep attachCanvas and presentFrame off the host test target so `ra-wasm` unit tests compile without Renderer::`attach_canvas`.

--- projects/bindings/ra-wasm/src/host/present.rs
+++ projects/bindings/ra-wasm/src/host/present.rs
@@ -1,33 +1,55 @@
 //! wgpu 画布呈现（Wasm 宿主）。
 //!
 //! `attach_canvas` 成功后 `supports_present` 为真；首帧清屏验证交换链。
+//! 表面附着与帧提交仅在 `wasm32` 编译；宿主测试目标只保留探测桩。
 
-use std::cell::RefCell;
-
-use ra_renderer::Renderer;
+#[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
+
+#[cfg(target_arch = "wasm32")]
+use std::cell::RefCell;
+#[cfg(target_arch = "wasm32")]
+use ra_renderer::Renderer;
+#[cfg(target_arch = "wasm32")]
 use web_sys::HtmlCanvasElement;
 
+#[cfg(target_arch = "wasm32")]
 struct PresentState {
     renderer: Renderer,
     ready: bool,
 }
 
+#[cfg(target_arch = "wasm32")]
 thread_local! {
     static PRESENT: RefCell<Option<PresentState>> = const { RefCell::new(None) };
 }
 
 /// 是否已附着可用的 wgpu 表面。
 pub fn is_ready() -> bool {
-    PRESENT.with(|slot| slot.borrow().as_ref().is_some_and(|s| s.ready))
+    #[cfg(target_arch = "wasm32")]
+    {
+        PRESENT.with(|slot| slot.borrow().as_ref().is_some_and(|s| s.ready))
+    }
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        false
+    }
 }
 
 /// 当前后端标签（未附着则为空）。
 pub fn backend_label() -> Option<&'static str> {
-    PRESENT.with(|slot| slot.borrow().as_ref().and_then(|s| s.renderer.backend_label()))
+    #[cfg(target_arch = "wasm32")]
+    {
+        PRESENT.with(|slot| slot.borrow().as_ref().and_then(|s| s.renderer.backend_label()))
+    }
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        None
+    }
 }
 
 /// 将 HTML canvas 附着为 wgpu 表面并提交一帧清屏。
+#[cfg(target_arch = "wasm32")]
 #[wasm_bindgen(js_name = attachCanvas)]
 pub async fn attach_canvas(canvas: HtmlCanvasElement) -> Result<(), JsValue> {
     if is_ready() {
@@ -49,6 +71,7 @@ pub async fn attach_canvas(canvas: HtmlCanvasElement) -> Result<(), JsValue> {
 }
 
 /// 调整已附着表面尺寸并重绘一帧。
+#[cfg(target_arch = "wasm32")]
 #[wasm_bindgen(js_name = resizePresent)]
 pub fn resize_present(width: u32, height: u32) -> Result<(), JsValue> {
     PRESENT.with(|slot| {
@@ -64,6 +87,7 @@ pub fn resize_present(width: u32, height: u32) -> Result<(), JsValue> {
 }
 
 /// 提交一帧（当前为清屏 / 已上传纹理的合成；无对局环）。
+#[cfg(target_arch = "wasm32")]
 #[wasm_bindgen(js_name = presentFrame)]
 pub fn present_frame() -> Result<(), JsValue> {
     PRESENT.with(|slot| {
@@ -78,6 +102,7 @@ pub fn present_frame() -> Result<(), JsValue> {
 }
 
 /// 当前 wgpu 后端标签字符串。
+#[cfg(target_arch = "wasm32")]
 #[wasm_bindgen(js_name = presentBackend)]
 pub fn present_backend() -> String {
     backend_label().unwrap_or("none").to_string()
