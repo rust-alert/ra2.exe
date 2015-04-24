5d5832c0fd30bd0d83788f60b7e6fee8254dcf0c
aster <aster@vers.site>
2015-04-24 06:24:57 +0000

    ✨ Project `exit_confirm_layout` from content snapshot
    Read dialog, prompt, and button rects from `exit_confirm_content_layout_tree` via `LayoutEngine` so transitional layout no longer recomputes DLU locally.

--- projects/engine/ra-layout/src/ui_layout/exit_confirm.rs
+++ projects/engine/ra-layout/src/ui_layout/exit_confirm.rs
@@ -1,7 +1,10 @@
 //! 退出确认对话框布局。
 
 use super::*;
-
+use crate::{
+    exit_confirm_content_layout_tree, LayoutEngine, LayoutSnapshot, Rect, RightPanelChrome,
+    Viewport,
+};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct ExitConfirmLayout {
@@ -13,31 +16,34 @@ pub struct ExitConfirmLayout {
     pub buttons: [RectPx; 2],
 }
 
-pub(super) fn modal_child(dialog: RectPx, local: RectPx) -> RectPx {
-    RectPx::new(dialog.x + local.x, dialog.y + local.y, local.w, local.h)
+fn rect_px(snap: &LayoutSnapshot, id: &str) -> RectPx {
+    let Rect {
+        x,
+        y,
+        width,
+        height,
+    } = snap.get(id).map(|e| e.layout.rect).unwrap_or_default();
+    RectPx::new(x as i32, y as i32, width as i32, height as i32)
 }
 
 /// 退出确认布局（相对壳层画布居中；底下仍是主菜单右栏）。
+///
+/// 交互几何来自 `exit_confirm_content_layout_tree`。
 pub fn exit_confirm_layout(_viewport_w: u32, _viewport_h: u32) -> ExitConfirmLayout {
-    let dialog = RectPx::new(
-        (((SHELL_BASE_W - EXIT_CONFIRM_DIALOG_W) + 1) / 2).max(0),
-        (((SHELL_BASE_H - EXIT_CONFIRM_DIALOG_H) + 1) / 2).max(0),
-        EXIT_CONFIRM_DIALOG_W,
-        EXIT_CONFIRM_DIALOG_H,
+    let chrome = RightPanelChrome::shell_defaults();
+    let snap = LayoutEngine.solve(
+        Viewport {
+            size: crate::shell_design_size(chrome),
+            ..Viewport::default()
+        },
+        &exit_confirm_content_layout_tree(chrome),
     );
     ExitConfirmLayout {
-        dialog,
-        prompt: modal_child(dialog, dlu_rect(40, 40, 220, 50)),
-        // 控件原点取 DLU，宽高取 `mnbttn` 画布（126×25），避免 125×24 格内居中错位。
+        dialog: rect_px(&snap, "dialog"),
+        prompt: rect_px(&snap, "prompt"),
         buttons: [
-            {
-                let r = modal_child(dialog, dlu_rect(207, 135, 83, 15));
-                RectPx::new(r.x, r.y, EXIT_CONFIRM_BUTTON_W, EXIT_CONFIRM_BUTTON_H)
-            },
-            {
-                let r = modal_child(dialog, dlu_rect(207, 175, 83, 15));
-                RectPx::new(r.x, r.y, EXIT_CONFIRM_BUTTON_W, EXIT_CONFIRM_BUTTON_H)
-            },
+            rect_px(&snap, EXIT_CONFIRM_BUTTON_IDS[0]),
+            rect_px(&snap, EXIT_CONFIRM_BUTTON_IDS[1]),
         ],
     }
 }
