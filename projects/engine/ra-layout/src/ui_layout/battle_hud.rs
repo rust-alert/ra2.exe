6926cc20e43b0aa3df5674323ab93d10eb6b10e1
aster <aster@vers.site>
2015-05-09 08:53:19 +0000

    ✨ Project `battle_hud_layout` from `battle_hud_layout_tree`
    Drive HUD slot rects through one viewport snapshot path.
    Keep `world_viewport` as a derived chrome inset helper.

--- projects/engine/ra-layout/src/ui_layout/battle_hud.rs
+++ projects/engine/ra-layout/src/ui_layout/battle_hud.rs
@@ -1,6 +1,7 @@
 //! 对局 HUD 布局。
 
 use super::*;
+use crate::{battle_hud_layout_tree, LayoutEngine, Size2, Viewport};
 
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -33,47 +34,44 @@ pub struct BattleHudLayout {
     pub diplo_btn: RectPx,
 }
 
+impl BattleHudLayout {
+    /// 世界层可视矩形：左起至侧栏左缘，上起至底栏上缘（不含右栏与底栏 chrome）。
+    pub fn world_viewport(&self) -> RectPx {
+        let w = self.sidebar.x.max(0);
+        let h = self.bottom_strip.y.max(0);
+        RectPx::new(0, 0, w, h)
+    }
+}
+
 /// 按视口计算对局 HUD 布局（右栏宽对齐壳层 `RIGHT_PANEL_W`）。
+///
+/// 几何投影自 `battle_hud_layout_tree` → `LayoutSnapshot`。
 pub fn battle_hud_layout(viewport_w: u32, viewport_h: u32) -> BattleHudLayout {
-    let w = viewport_w.max(1) as i32;
-    let h = viewport_h.max(1) as i32;
-    let panel_w = RIGHT_PANEL_W.min(w).max(1);
-    let panel_x = (w - panel_w).max(0);
-    let sidebar = RectPx::new(panel_x, 0, panel_w, h);
-    let credits_h = 28.min(h).max(1);
-    let credits = RectPx::new(panel_x, 0, panel_w, credits_h);
-    let top_h = 24.min((h - credits_h).max(1));
-    let top = RectPx::new(panel_x, credits.y + credits.h, panel_w, top_h);
-    let radar_h = 120.min((h - credits_h - top_h).max(1));
-    let radar = RectPx::new(panel_x, top.y + top.h, panel_w, radar_h);
-    let side1_h = 36.min((h - (radar.y + radar.h)).max(1));
-    let side1 = RectPx::new(panel_x, radar.y + radar.h, panel_w, side1_h);
-    let bottom_h = 48.min(h / 8).max(24).min(h);
-    let side3_h = 28.min((h - bottom_h - (side1.y + side1.h)).max(1));
-    let cameo_bottom = (h - bottom_h - side3_h).max(side1.y + side1.h);
-    let cameo_band = RectPx::new(panel_x, side1.y + side1.h, panel_w, (cameo_bottom - (side1.y + side1.h)).max(1));
-    let side3 = RectPx::new(panel_x, cameo_band.y + cameo_band.h, panel_w, side3_h);
-    let addon = RectPx::new(panel_x, side3.y, panel_w, side3_h.min(20).max(1));
-    let btn_w = ((panel_w - 12) / 2).max(1);
-    let btn_h = 22.min(side1_h).max(1);
-    let repair = RectPx::new(panel_x + 4, side1.y + 4, btn_w, btn_h);
-    let sell = RectPx::new(panel_x + 8 + btn_w, side1.y + 4, btn_w, btn_h);
-    let bottom_strip = RectPx::new(0, h - bottom_h, w, bottom_h);
-    let opt_btn = RectPx::new(8, bottom_strip.y + 8, 72, (bottom_h - 16).max(1));
-    let diplo_btn = RectPx::new(88, bottom_strip.y + 8, 72, (bottom_h - 16).max(1));
+    let w = viewport_w.max(1) as f32;
+    let h = viewport_h.max(1) as f32;
+    let snap = LayoutEngine.solve(
+        Viewport {
+            size: Size2 {
+                width: w,
+                height: h,
+            },
+            ..Viewport::default()
+        },
+        &battle_hud_layout_tree(viewport_w, viewport_h),
+    );
     BattleHudLayout {
-        sidebar,
-        credits,
-        top,
-        radar,
-        side1,
-        cameo_band,
-        side3,
-        addon,
-        repair,
-        sell,
-        bottom_strip,
-        opt_btn,
-        diplo_btn,
+        sidebar: rect_px_from_snapshot(&snap, "sidebar"),
+        credits: rect_px_from_snapshot(&snap, "credits"),
+        top: rect_px_from_snapshot(&snap, "top"),
+        radar: rect_px_from_snapshot(&snap, "radar"),
+        side1: rect_px_from_snapshot(&snap, "side1"),
+        cameo_band: rect_px_from_snapshot(&snap, "cameo_band"),
+        side3: rect_px_from_snapshot(&snap, "side3"),
+        addon: rect_px_from_snapshot(&snap, "addon"),
+        repair: rect_px_from_snapshot(&snap, "repair"),
+        sell: rect_px_from_snapshot(&snap, "sell"),
+        bottom_strip: rect_px_from_snapshot(&snap, "bottom_strip"),
+        opt_btn: rect_px_from_snapshot(&snap, "opt_btn"),
+        diplo_btn: rect_px_from_snapshot(&snap, "diplo_btn"),
     }
 }
