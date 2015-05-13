23382eb0f62dc22e417bcf9006129ad660d0e4fc
aster <aster@vers.site>
2015-05-13 12:42:06 +0000

    ✨ Add `shell_page_layout_tree` for chrome plus rail
    Combine shared shell chrome and right-rail buttons into one layout tree.
    Keep the separate chrome and rail helpers for transitional callers.

--- projects/engine/ra-layout/src/lib.rs
+++ projects/engine/ra-layout/src/lib.rs
@@ -18,8 +18,9 @@ pub use snapshot::{HitRegion, HitTestMode, LayoutBox, LayoutElement, LayoutSnaps
 pub use reference::{
     battle_hud_layout_tree, campaign_content_layout_tree, dialog_layout_tree,
     exit_confirm_content_layout_tree, mul_div_round, resolve_control_desc, resolve_dialog_template,
-    right_rail_buttons_layout_tree, shell_chrome_layout_tree, shell_design_size, DluRect,
-    FontBaseUnits, LegacyReference, LegacyRole, LegacySource, MS_SANS_SERIF_8PT,
+    right_rail_buttons_layout_tree, shell_chrome_layout_tree, shell_design_size,
+    shell_page_layout_tree, DluRect, FontBaseUnits, LegacyReference, LegacyRole, LegacySource,
+    MS_SANS_SERIF_8PT,
 };
 pub use spec::{fixed_rect_leaf, root_with_fixed_children, LayoutId, LayoutNode};
 pub use policy::{
--- projects/engine/ra-layout/src/reference/shell_chrome.rs
+++ projects/engine/ra-layout/src/reference/shell_chrome.rs
@@ -22,12 +22,11 @@ const TOOLTIP_W: f32 = 455.0;
 const TOOLTIP_H: f32 = 20.0;
 const TOOLTIP_BOTTOM_GAP: f32 = 1.0;
 
-/// 壳层共享 chrome（面板条带、影片区、标题、提示），不含页面按钮。
-pub fn shell_chrome_layout_tree(chrome: RightPanelChrome) -> LayoutNode {
+fn shell_chrome_children(chrome: RightPanelChrome) -> Vec<LayoutNode> {
     let panel_x = chrome.panel_x();
     let movie_w = panel_x;
     let bottom_y = chrome.panel_bottom_y();
-    let children = vec![
+    vec![
         fixed_rect_leaf(
             "panel_top",
             Rect::from_xywh(panel_x, 0.0, chrome.panel_w, chrome.panel_top_h),
@@ -62,19 +61,14 @@ pub fn shell_chrome_layout_tree(chrome: RightPanelChrome) -> LayoutNode {
                 TOOLTIP_H,
             ),
         ),
-    ];
-    root_with_fixed_children("shell_chrome", shell_design_size(chrome), children)
+    ]
 }
 
-/// 右栏连续平铺格 + 可选贴底盖按钮。
-///
-/// `stacked_ids` 从 tile 0 起依次占格；`bottom_id` 若有则贴底盖上沿。
-pub fn right_rail_buttons_layout_tree(
-    root_id: impl Into<String>,
+fn right_rail_button_children(
     stacked_ids: &[&str],
     bottom_id: Option<&str>,
     chrome: RightPanelChrome,
-) -> LayoutNode {
+) -> Vec<LayoutNode> {
     let mut children = Vec::with_capacity(stacked_ids.len() + bottom_id.is_some() as usize);
     for (i, id) in stacked_ids.iter().enumerate() {
         let rect = Rect::from_xywh(
@@ -88,6 +82,43 @@ pub fn right_rail_buttons_layout_tree(
     if let Some(id) = bottom_id {
         children.push(fixed_rect_leaf(id, bottom_cover_button(chrome)));
     }
+    children
+}
+
+/// 壳层共享 chrome（面板条带、影片区、标题、提示），不含页面按钮。
+pub fn shell_chrome_layout_tree(chrome: RightPanelChrome) -> LayoutNode {
+    root_with_fixed_children(
+        "shell_chrome",
+        shell_design_size(chrome),
+        shell_chrome_children(chrome),
+    )
+}
+
+/// 右栏连续平铺格 + 可选贴底盖按钮。
+///
+/// `stacked_ids` 从 tile 0 起依次占格；`bottom_id` 若有则贴底盖上沿。
+pub fn right_rail_buttons_layout_tree(
+    root_id: impl Into<String>,
+    stacked_ids: &[&str],
+    bottom_id: Option<&str>,
+    chrome: RightPanelChrome,
+) -> LayoutNode {
+    root_with_fixed_children(
+        root_id,
+        shell_design_size(chrome),
+        right_rail_button_children(stacked_ids, bottom_id, chrome),
+    )
+}
+
+/// 壳层页面：共享 chrome + 右栏按钮，一次求解。
+pub fn shell_page_layout_tree(
+    root_id: impl Into<String>,
+    stacked_ids: &[&str],
+    bottom_id: Option<&str>,
+    chrome: RightPanelChrome,
+) -> LayoutNode {
+    let mut children = shell_chrome_children(chrome);
+    children.extend(right_rail_button_children(stacked_ids, bottom_id, chrome));
     root_with_fixed_children(root_id, shell_design_size(chrome), children)
 }
 
@@ -160,6 +191,37 @@ mod tests {
             assert_eq!(got.y as i32, cell.y, "{id} y");
             assert_eq!(got.width as i32, cell.w, "{id} w");
             assert_eq!(got.height as i32, cell.h, "{id} h");
+        }
+    }
+
+    #[test]
+    fn shell_page_tree_matches_chrome_and_buttons() {
+        let chrome = RightPanelChrome::shell_defaults();
+        let ids = [
+            "single_player",
+            "ww_online",
+            "network",
+            "movies",
+            "options",
+            "exit",
+        ];
+        let snap = LayoutEngine.solve(
+            Viewport {
+                size: shell_design_size(chrome),
+                ..Viewport::default()
+            },
+            &shell_page_layout_tree("main_menu", &ids[..5], Some(ids[5]), chrome),
+        );
+        let legacy = crate::ui_layout::main_menu_layout(800, 600);
+        assert_eq!(
+            snap.get("title").unwrap().layout.rect.width as i32,
+            legacy.title.w
+        );
+        for (i, id) in ids.iter().enumerate() {
+            let cell = legacy.buttons[i];
+            let got = snap.get(id).expect(id).layout.rect;
+            assert_eq!(got.x as i32, cell.x, "{id} x");
+            assert_eq!(got.y as i32, cell.y, "{id} y");
         }
     }
 }
