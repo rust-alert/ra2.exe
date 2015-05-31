368c1dd98b3c8646337c4b6742f3d281c7b41ddc
aster <aster@vers.site>
2015-05-31 05:48:29 +0000

    ✨ Add `load_screen_layout_tree` for loading page slots
    Encode text, progress, and retry/cancel rects on the 800x600 design canvas.
    Lock compose-constant and fraction-button parity with a unit test.

---
+++ projects/engine/ra-layout/src/reference/load_screen.rs
@@ -0,0 +1,94 @@
+//! 装载页内容区固定设计矩形 → `LayoutNode`（800×600）。
+
+use crate::{
+    geometry::{Rect, Size2},
+    policy::RightPanelChrome,
+    reference::from_template::shell_design_size,
+    spec::{fixed_rect_leaf, root_with_fixed_children, LayoutNode},
+};
+
+/// 装载失败时操作钮 id。
+pub const LOAD_SCREEN_BUTTON_IDS: [&str; 2] = ["retry", "cancel"];
+
+fn rect_i(x: i32, y: i32, w: i32, h: i32) -> Rect {
+    Rect::from_xywh(x as f32, y as f32, w as f32, h as f32)
+}
+
+/// 归一化命中框 → 800×600 设计像素。
+fn frac_rect(x0: f32, y0: f32, x1: f32, y1: f32, chrome: RightPanelChrome) -> Rect {
+    let w = chrome.shell_w;
+    let h = chrome.shell_h;
+    Rect::from_xywh(
+        (x0 * w).round(),
+        (y0 * h).round(),
+        ((x1 - x0) * w).round().max(1.0),
+        ((y1 - y0) * h).round().max(1.0),
+    )
+}
+
+/// 装载页：文案槽、进度条原点、失败时重试/取消。
+pub fn load_screen_layout_tree(chrome: RightPanelChrome) -> LayoutNode {
+    let children = vec![
+        // 对照原版截图的 800×600 像素映射（与过渡期 compose 常量一致）。
+        fixed_rect_leaf("special", rect_i(54, 106, 190, 22)),
+        fixed_rect_leaf("brief", rect_i(48, 134, 340, 200)),
+        fixed_rect_leaf("name", rect_i(648, 538, 120, 24)),
+        fixed_rect_leaf("status", rect_i(56, 310, 160, 20)),
+        fixed_rect_leaf("progress", rect_i(56, 332, 200, 16)),
+        fixed_rect_leaf("player_flag", rect_i(150, 324, 40, 24)),
+        fixed_rect_leaf("player_name", rect_i(202, 328, 120, 20)),
+        fixed_rect_leaf(
+            LOAD_SCREEN_BUTTON_IDS[0],
+            frac_rect(0.30, 0.88, 0.50, 0.96, chrome),
+        ),
+        fixed_rect_leaf(
+            LOAD_SCREEN_BUTTON_IDS[1],
+            frac_rect(0.54, 0.88, 0.74, 0.96, chrome),
+        ),
+    ];
+    root_with_fixed_children("load_screen", shell_design_size(chrome), children)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{LayoutEngine, Viewport};
+
+    #[test]
+    fn load_screen_tree_matches_compose_constants() {
+        let chrome = RightPanelChrome::shell_defaults();
+        let snap = LayoutEngine.solve(
+            Viewport {
+                size: Size2 {
+                    width: chrome.shell_w,
+                    height: chrome.shell_h,
+                },
+                ..Viewport::default()
+            },
+            &load_screen_layout_tree(chrome),
+        );
+        for (id, x, y, w, h) in [
+            ("special", 54, 106, 190, 22),
+            ("brief", 48, 134, 340, 200),
+            ("name", 648, 538, 120, 24),
+            ("status", 56, 310, 160, 20),
+            ("progress", 56, 332, 200, 16),
+            ("player_flag", 150, 324, 40, 24),
+            ("player_name", 202, 328, 120, 20),
+        ] {
+            let got = snap.get(id).expect(id).layout.rect;
+            assert_eq!(got.x as i32, x, "{id} x");
+            assert_eq!(got.y as i32, y, "{id} y");
+            assert_eq!(got.width as i32, w, "{id} w");
+            assert_eq!(got.height as i32, h, "{id} h");
+        }
+        let retry = snap.get("retry").unwrap().layout.rect;
+        assert_eq!(retry.x as i32, 240);
+        assert_eq!(retry.y as i32, 528);
+        assert_eq!(retry.width as i32, 160);
+        assert_eq!(retry.height as i32, 48);
+        let cancel = snap.get("cancel").unwrap().layout.rect;
+        assert_eq!(cancel.x as i32, 432);
+        assert_eq!(cancel.y as i32, 528);
+    }
+}
--- projects/engine/ra-layout/src/reference/mod.rs
+++ projects/engine/ra-layout/src/reference/mod.rs
@@ -6,6 +6,7 @@ mod dlu;
 mod exit_confirm_page;
 mod from_template;
 mod legacy;
+mod load_screen;
 mod shell_chrome;
 
 pub use battle_hud::battle_hud_layout_tree;
@@ -16,6 +17,7 @@ pub use from_template::{
     dialog_layout_tree, resolve_control_desc, resolve_dialog_template, shell_design_size,
 };
 pub use legacy::{LegacyReference, LegacyRole, LegacySource};
+pub use load_screen::{load_screen_layout_tree, LOAD_SCREEN_BUTTON_IDS};
 pub use shell_chrome::{
     right_rail_buttons_layout_tree, shell_chrome_layout_tree, shell_page_layout_tree,
 };
