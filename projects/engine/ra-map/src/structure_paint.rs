a0eae564556d6994cb983fa707e959115b2b2946
aster <aster@vers.site>
2015-07-24 19:16:50 +0000

    ✨ Paint structure `ActiveAnim` layers in map preview
    Draw `ActiveAnim` and `ActiveAnimTwo` on top of the building body so tech oil derrick flags and pumps appear. Honor Remapable=no and signed SHP frame offsets.

--- projects/engine/ra-map/src/structure_paint.rs
+++ projects/engine/ra-map/src/structure_paint.rs
@@ -12,6 +12,8 @@ use crate::{
 };
 
 /// 叠画 `[Structures]`。`remap_owner(base, owner)` 返回房屋色调色板。
+///
+/// 除主体外还会叠 `ActiveAnim` / `ActiveAnimTwo`（如油田旗帜 `CAOILD_F`）。
 pub fn paint_map_structures(
     source: &dyn AssetSource,
     map: &MapInfo,
@@ -40,68 +42,149 @@ pub fn paint_map_structures(
     };
 
     let mut shp_cache: HashMap<String, ShpFile> = HashMap::new();
-    let mut blit_cache: HashMap<(String, String), TileBlit> = HashMap::new();
+    // (layer_stem, owner, frame, z_adjust) → blit
+    let mut blit_cache: HashMap<(String, String, u16, i32), TileBlit> = HashMap::new();
     let mut items: Vec<(u16, u16, TileBlit)> = Vec::new();
 
     for ent in structures {
-        let image_key = art.as_ref().and_then(|a| a.get(&ent.type_id, "Image")).unwrap_or(ent.type_id.as_str()).to_ascii_uppercase();
-        let cache_key = (image_key.clone(), ent.owner.clone());
-        if let Some(blit) = blit_cache.get(&cache_key) {
-            items.push((ent.x, ent.y, blit.clone()));
-            continue;
-        }
-        let new_theater = art.as_ref().and_then(|a| a.get(&ent.type_id, "NewTheater")).is_some_and(|v| v.eq_ignore_ascii_case("yes"));
-        let candidates = if new_theater {
-            vec![new_theater_shp_name(&image_key, map.theater), format!("{}.shp", image_key.to_ascii_lowercase())]
-        }
-        else {
-            vec![format!("{}.shp", image_key.to_ascii_lowercase()), new_theater_shp_name(&image_key, map.theater)]
-        };
+        let art_section = art
+            .as_ref()
+            .and_then(|a| {
+                let image_key = a.get(&ent.type_id, "Image").unwrap_or(ent.type_id.as_str());
+                if a.section(image_key).is_some() {
+                    Some(image_key.to_ascii_uppercase())
+                } else if a.section(&ent.type_id).is_some() {
+                    Some(ent.type_id.to_ascii_uppercase())
+                } else {
+                    None
+                }
+            })
+            .unwrap_or_else(|| ent.type_id.to_ascii_uppercase());
 
-        let mut loaded: Option<String> = None;
-        for file in &candidates {
-            if shp_cache.contains_key(file) {
-                loaded = Some(file.clone());
-                break;
+        let remapable = art
+            .as_ref()
+            .and_then(|a| a.get(&art_section, "Remapable"))
+            .is_none_or(|v| !v.eq_ignore_ascii_case("no"));
+        let pal = if remapable { remap_owner(&obj_pal, &ent.owner) } else { obj_pal.clone() };
+
+        let body_key = art.as_ref().and_then(|a| a.get(&art_section, "Image")).unwrap_or(art_section.as_str()).to_ascii_uppercase();
+        let body_new_theater =
+            art.as_ref().and_then(|a| a.get(&art_section, "NewTheater")).is_some_and(|v| v.eq_ignore_ascii_case("yes"));
+        if let Some(blit) = load_structure_blit(
+            source,
+            map,
+            &body_key,
+            body_new_theater,
+            0,
+            0,
+            &pal,
+            &mut shp_cache,
+            &mut blit_cache,
+            &ent.owner,
+        ) {
+            items.push((ent.x, ent.y, blit));
+        }
+
+        // 活动层：泵机 / 旗帜等。预览取 Start 帧（未接逐帧驱动前为静止姿态）。
+        for (anim_key, z_key) in [("ActiveAnim", "ActiveAnimZAdjust"), ("ActiveAnimTwo", "ActiveAnimTwoZAdjust")] {
+            let Some(anim_name) = art.as_ref().and_then(|a| a.get(&art_section, anim_key)).map(str::to_ascii_uppercase)
+            else {
+                continue;
+            };
+            let z_adjust = art.as_ref().and_then(|a| a.get(&art_section, z_key)).and_then(parse_i32).unwrap_or(0);
+            let anim_image = art.as_ref().and_then(|a| a.get(&anim_name, "Image")).unwrap_or(anim_name.as_str()).to_ascii_uppercase();
+            let anim_new_theater =
+                art.as_ref().and_then(|a| a.get(&anim_name, "NewTheater")).is_some_and(|v| v.eq_ignore_ascii_case("yes"));
+            let start = art.as_ref().and_then(|a| a.get(&anim_name, "Start")).and_then(parse_u16).unwrap_or(0);
+            let anim_remapable = art
+                .as_ref()
+                .and_then(|a| a.get(&anim_name, "Remapable"))
+                .map(|v| !v.eq_ignore_ascii_case("no"))
+                .unwrap_or(remapable);
+            let anim_pal = if anim_remapable { remap_owner(&obj_pal, &ent.owner) } else { obj_pal.clone() };
+            if let Some(blit) = load_structure_blit(
+                source,
+                map,
+                &anim_image,
+                anim_new_theater,
+                start,
+                z_adjust,
+                &anim_pal,
+                &mut shp_cache,
+                &mut blit_cache,
+                &ent.owner,
+            ) {
+                items.push((ent.x, ent.y, blit));
             }
-            let Ok(bytes) = source.read(file)
-            else {
-                continue;
-            };
-            let Ok(shp) = ShpFile::parse(&bytes)
-            else {
-                continue;
-            };
-            shp_cache.insert(file.clone(), shp);
-            loaded = Some(file.clone());
-            break;
         }
-        let Some(file) = loaded
-        else {
-            continue;
-        };
-        let Some(shp) = shp_cache.get(&file)
-        else {
-            continue;
-        };
-        let Some(frame) = shp.frames.first()
-        else {
-            continue;
-        };
-        if frame.frame_width == 0 || frame.frame_height == 0 {
-            continue;
-        }
-        let pal = remap_owner(&obj_pal, &ent.owner);
-        let blit = TileBlit {
-            width: u32::from(frame.frame_width),
-            height: u32::from(frame.frame_height),
-            offset_x: i32::from(frame.frame_x),
-            offset_y: i32::from(frame.frame_y),
-            rgba: frame.to_rgba(&pal),
-        };
-        blit_cache.insert(cache_key, blit.clone());
-        items.push((ent.x, ent.y, blit));
     }
 
     paint_cell_sprites(image, &items, z_at)
+}
+
+fn load_structure_blit(
+    source: &dyn AssetSource,
+    map: &MapInfo,
+    image_key: &str,
+    new_theater: bool,
+    frame_idx: u16,
+    z_adjust: i32,
+    pal: &Palette,
+    shp_cache: &mut HashMap<String, ShpFile>,
+    blit_cache: &mut HashMap<(String, String, u16, i32), TileBlit>,
+    owner: &str,
+) -> Option<TileBlit> {
+    let cache_key = (image_key.to_owned(), owner.to_owned(), frame_idx, z_adjust);
+    if let Some(blit) = blit_cache.get(&cache_key) {
+        return Some(blit.clone());
+    }
+
+    let candidates = if new_theater {
+        vec![new_theater_shp_name(image_key, map.theater), format!("{}.shp", image_key.to_ascii_lowercase())]
+    } else {
+        vec![format!("{}.shp", image_key.to_ascii_lowercase()), new_theater_shp_name(image_key, map.theater)]
+    };
+
+    let mut loaded: Option<String> = None;
+    for file in &candidates {
+        if shp_cache.contains_key(file) {
+            loaded = Some(file.clone());
+            break;
+        }
+        let Ok(bytes) = source.read(file)
+        else {
+            continue;
+        };
+        let Ok(shp) = ShpFile::parse(&bytes)
+        else {
+            continue;
+        };
+        shp_cache.insert(file.clone(), shp);
+        loaded = Some(file.clone());
+        break;
+    }
+    let file = loaded?;
+    let shp = shp_cache.get(&file)?;
+    let frame = shp.frames.get(usize::from(frame_idx)).or_else(|| shp.frames.first())?;
+    if frame.frame_width == 0 || frame.frame_height == 0 {
+        return None;
+    }
+    let blit = TileBlit {
+        width: u32::from(frame.frame_width),
+        height: u32::from(frame.frame_height),
+        offset_x: i32::from(frame.frame_x as i16),
+        // ZAdjust 按像素偏置叠画 Y，使旗帜等活动层相对主体前后正确。
+        offset_y: i32::from(frame.frame_y as i16) + z_adjust,
+        rgba: frame.to_rgba(pal),
+    };
+    blit_cache.insert(cache_key, blit.clone());
+    Some(blit)
+}
+
+fn parse_i32(raw: &str) -> Option<i32> {
+    raw.trim().parse().ok()
+}
+
+fn parse_u16(raw: &str) -> Option<u16> {
+    raw.trim().parse().ok()
 }
--- projects/engine/ra-map/tests/structure_paint.rs
+++ projects/engine/ra-map/tests/structure_paint.rs
@@ -1,4 +1,6 @@
-use ra_map::{MapInfo, TerrainImage, paint_map_structures};
+use std::collections::HashMap;
+
+use ra_map::{MapEntity, MapEntityKind, MapInfo, TerrainImage, paint_map_structures};
 use ra_types::{AssetSource, GameEdition, RaError, RaResult};
 
 struct EmptySource;
@@ -8,9 +10,93 @@ impl AssetSource for EmptySource {
     }
 }
 
+struct MapSource {
+    files: HashMap<String, Vec<u8>>,
+}
+
+impl AssetSource for MapSource {
+    fn read(&self, relative: &str) -> RaResult<Vec<u8>> {
+        self.files.get(&relative.to_ascii_lowercase()).cloned().ok_or_else(|| RaError::MissingFile(relative.to_string()))
+    }
+}
+
+fn raw_one_pixel_shp(index: u8) -> Vec<u8> {
+    let mut data = Vec::new();
+    data.extend_from_slice(&0u16.to_le_bytes());
+    data.extend_from_slice(&1u16.to_le_bytes());
+    data.extend_from_slice(&1u16.to_le_bytes());
+    data.extend_from_slice(&1u16.to_le_bytes());
+    data.extend_from_slice(&0u16.to_le_bytes());
+    data.extend_from_slice(&0u16.to_le_bytes());
+    data.extend_from_slice(&1u16.to_le_bytes());
+    data.extend_from_slice(&1u16.to_le_bytes());
+    data.push(0);
+    data.extend_from_slice(&[0, 0, 0]);
+    data.extend_from_slice(&[0, 0, 0, 0]);
+    data.extend_from_slice(&0u32.to_le_bytes());
+    data.extend_from_slice(&(32u32).to_le_bytes());
+    data.push(index);
+    data
+}
+
+fn solid_index_pal(index: usize, r6: u8, g6: u8, b6: u8) -> Vec<u8> {
+    let mut data = vec![0u8; 768];
+    let o = index * 3;
+    data[o] = r6;
+    data[o + 1] = g6;
+    data[o + 2] = b6;
+    data
+}
+
 #[test]
 fn empty_structures_noop() {
     let map = MapInfo::empty(GameEdition::Ra2, "t");
     let mut image = TerrainImage::blank(1, 1);
     assert_eq!(paint_map_structures(&EmptySource, &map, &mut image, "art.ini", &|p, _| p.clone()), 0);
+}
+
+#[test]
+fn oil_derrick_paints_active_anim_two_flag() {
+    let art = b"\
+[CAOILD]\n\
+Remapable=no\n\
+NewTheater=yes\n\
+ActiveAnim=CAOILD_A\n\
+ActiveAnimTwo=CAOILD_F\n\
+ActiveAnimTwoZAdjust=-50\n\
+\n\
+[CAOILD_A]\n\
+Image=CAOILD_A\n\
+NewTheater=yes\n\
+Start=0\n\
+\n\
+[CAOILD_F]\n\
+Image=CAOILD_F\n\
+NewTheater=yes\n\
+Start=0\n\
+";
+    let mut files = HashMap::new();
+    files.insert("art.ini".into(), art.to_vec());
+    files.insert("unittem.pal".into(), solid_index_pal(5, 63, 63, 0));
+    // NewTheater temperate：第二字母 → t
+    files.insert("ctoild.shp".into(), raw_one_pixel_shp(5));
+    files.insert("ctoild_a.shp".into(), raw_one_pixel_shp(5));
+    files.insert("ctoild_f.shp".into(), raw_one_pixel_shp(5));
+
+    let mut map = MapInfo::empty(GameEdition::Ra2, "t");
+    map.entities.push(MapEntity {
+        kind: MapEntityKind::Structure,
+        owner: "Neutral".into(),
+        type_id: "CAOILD".into(),
+        health: 256,
+        x: 5,
+        y: 0,
+        facing: 0,
+        sub_cell: 0,
+    });
+
+    let source = MapSource { files };
+    let mut image = TerrainImage::blank(256, 256);
+    let painted = paint_map_structures(&source, &map, &mut image, "art.ini", &|p, _| p.clone());
+    assert_eq!(painted, 3, "body + pump ActiveAnim + flag ActiveAnimTwo");
 }
