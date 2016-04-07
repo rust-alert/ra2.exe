0103e505ddde0e4a7da0d49a855814cf64f58aa8
aster <aster@vers.site>
2016-04-07 06:27:57 +0000

    🧪 Assert ore-tree idle frame and FA2 full-canvas anchor
    Cover `SpawnsTiberium` staying on frame 0 under present clocks, `StaticOnly` Idle paint, and the 84x56 canvas landing with -3 Y. Move continuous clock coverage onto non-spawner looping terrain.

--- projects/engine/ra-map/tests/terrain_paint.rs
+++ projects/engine/ra-map/tests/terrain_paint.rs
@@ -187,24 +187,21 @@ fn animation_rate_converts_logic_frames_to_ms() {
 }
 
 #[test]
-fn animated_terrain_selects_body_frame_by_clock() {
-    // 帧 0 索引 5→绿；帧 1 索引 6→红。`AnimationRate=3` 时 200ms 切到第 1 帧。
+fn looping_animated_terrain_selects_body_frame_by_clock() {
+    // 非产矿旗帜类：帧 0 绿、帧 1 红。`AnimationRate=3` 时 200ms 切到第 1 帧。
     let mut pal = solid_index_pal(5, 0, 63, 0);
     pal[6 * 3] = 63;
     pal[6 * 3 + 1] = 0;
     pal[6 * 3 + 2] = 0;
 
     let mut files = HashMap::new();
-    files.insert("art.ini".into(), b"[TIBTRE01]\nTheater=yes\n".to_vec());
-    files.insert(
-        "rules.ini".into(),
-        b"[TIBTRE01]\nIsAnimated=yes\nAnimationRate=3\nSpawnsTiberium=yes\n".to_vec(),
-    );
-    files.insert("unittem.pal".into(), pal);
-    files.insert("isotem.pal".into(), solid_index_pal(5, 40, 40, 40));
-    files.insert("tibtre01.tem".into(), multi_frame_shp(&[5, 6]));
+    files.insert("art.ini".into(), b"[FLAG01]\nTheater=yes\n".to_vec());
+    files.insert("rules.ini".into(), b"[FLAG01]\nIsAnimated=yes\nAnimationRate=3\n".to_vec());
+    files.insert("isotem.pal".into(), pal);
+    files.insert("flag01.tem".into(), multi_frame_shp(&[5, 6]));
     let source = MapSource { files };
-    let map = tibtre_map();
+    let mut map = MapInfo::empty(GameEdition::Ra2, "t");
+    map.terrain_objects = vec![TerrainObject { x: 5, y: 0, name: "FLAG01".into() }];
 
     let mut image0 = TerrainImage::blank(256, 256);
     assert_eq!(paint_map_terrain_objects(&source, &map, &mut image0, "art.ini", "rules.ini", clock(0)), 1);
@@ -215,6 +212,52 @@ fn animated_terrain_selects_body_frame_by_clock() {
     assert_eq!(paint_map_terrain_objects(&source, &map, &mut image1, "art.ini", "rules.ini", clock(200)), 1);
     let red = first_opaque(&image1);
     assert!(red[0] > red[1], "clock 200ms should paint frame 1 red, got {red:?}");
+}
+
+#[test]
+fn spawns_tiberium_stays_on_idle_frame_zero() {
+    // 矿柱是条件动画：即使有时钟也不循环，始终 Idle 第 0 帧（绿）。
+    let mut pal = solid_index_pal(5, 0, 63, 0);
+    pal[6 * 3] = 63;
+    pal[6 * 3 + 1] = 0;
+    pal[6 * 3 + 2] = 0;
+
+    let mut files = HashMap::new();
+    files.insert("art.ini".into(), b"[TIBTRE01]\nTheater=yes\n".to_vec());
+    files.insert(
+        "rules.ini".into(),
+        b"[TIBTRE01]\nIsAnimated=yes\nAnimationRate=3\nSpawnsTiberium=yes\nAnimationProbability=.5\n".to_vec(),
+    );
+    files.insert("unittem.pal".into(), pal);
+    files.insert("tibtre01.tem".into(), multi_frame_shp(&[5, 6]));
+    let source = MapSource { files };
+    let map = tibtre_map();
+
+    assert!(collect_terrain_anim_bank(&source, &map, "art.ini", "rules.ini").is_empty());
+
+    let mut image0 = TerrainImage::blank(256, 256);
+    assert_eq!(paint_map_terrain_objects(&source, &map, &mut image0, "art.ini", "rules.ini", clock(0)), 1);
+    let green0 = first_opaque(&image0);
+    assert!(green0[1] > green0[0], "idle frame 0 green, got {green0:?}");
+
+    let mut image1 = TerrainImage::blank(256, 256);
+    assert_eq!(paint_map_terrain_objects(&source, &map, &mut image1, "art.ini", "rules.ini", clock(200)), 1);
+    let green1 = first_opaque(&image1);
+    assert!(green1[1] > green1[0], "clock must not advance ore-tree idle frame, got {green1:?}");
+
+    let mut image_static = TerrainImage::blank(256, 256);
+    assert_eq!(
+        paint_map_terrain_objects(
+            &source,
+            &map,
+            &mut image_static,
+            "art.ini",
+            "rules.ini",
+            TerrainPaintMode::StaticOnly
+        ),
+        1,
+        "StaticOnly must still paint Idle ore trees"
+    );
 }
 
 #[test]
@@ -238,14 +281,15 @@ fn static_terrain_ignores_anim_clock() {
 }
 
 #[test]
-fn static_only_skips_animated_terrain() {
+fn static_only_skips_looping_animated_terrain() {
     let mut files = HashMap::new();
-    files.insert("art.ini".into(), b"[TIBTRE01]\nTheater=yes\n".to_vec());
-    files.insert("rules.ini".into(), b"[TIBTRE01]\nIsAnimated=yes\nAnimationRate=3\n".to_vec());
+    files.insert("art.ini".into(), b"[FLAG01]\nTheater=yes\n".to_vec());
+    files.insert("rules.ini".into(), b"[FLAG01]\nIsAnimated=yes\nAnimationRate=3\n".to_vec());
     files.insert("isotem.pal".into(), solid_index_pal(5, 0, 63, 0));
-    files.insert("tibtre01.tem".into(), multi_frame_shp(&[5, 6]));
+    files.insert("flag01.tem".into(), multi_frame_shp(&[5, 6]));
     let source = MapSource { files };
-    let map = tibtre_map();
+    let mut map = MapInfo::empty(GameEdition::Ra2, "t");
+    map.terrain_objects = vec![TerrainObject { x: 5, y: 0, name: "FLAG01".into() }];
     let mut image = TerrainImage::blank(256, 256);
     assert_eq!(
         paint_map_terrain_objects(&source, &map, &mut image, "art.ini", "rules.ini", TerrainPaintMode::StaticOnly),
@@ -254,35 +298,30 @@ fn static_only_skips_animated_terrain() {
 }
 
 #[test]
-fn terrain_anim_bank_records_hit_and_paints_by_clock() {
+fn terrain_anim_bank_records_looping_hit_and_paints_by_clock() {
     let mut pal = solid_index_pal(5, 0, 63, 0);
     pal[6 * 3] = 63;
     pal[6 * 3 + 1] = 0;
     pal[6 * 3 + 2] = 0;
 
     let mut files = HashMap::new();
-    files.insert("art.ini".into(), b"[TIBTRE01]\nTheater=yes\n".to_vec());
-    files.insert(
-        "rules.ini".into(),
-        b"[TIBTRE01]\nIsAnimated=yes\nAnimationRate=3\nSpawnsTiberium=yes\n".to_vec(),
-    );
-    files.insert("unittem.pal".into(), pal);
-    files.insert("isotem.pal".into(), solid_index_pal(5, 40, 40, 40));
-    files.insert("tibtre01.tem".into(), multi_frame_shp(&[5, 6]));
+    files.insert("art.ini".into(), b"[FLAG01]\nTheater=yes\n".to_vec());
+    files.insert("rules.ini".into(), b"[FLAG01]\nIsAnimated=yes\nAnimationRate=3\n".to_vec());
+    files.insert("isotem.pal".into(), pal);
+    files.insert("flag01.tem".into(), multi_frame_shp(&[5, 6]));
     let source = MapSource { files };
-    let map = tibtre_map();
+    let mut map = MapInfo::empty(GameEdition::Ra2, "t");
+    map.terrain_objects = vec![TerrainObject { x: 5, y: 0, name: "FLAG01".into() }];
 
     let bank = collect_terrain_anim_bank(&source, &map, "art.ini", "rules.ini");
     assert_eq!(bank.layers.len(), 1);
     let layer = &bank.layers[0];
-    assert_eq!(layer.file, "tibtre01.tem");
+    assert_eq!(layer.file, "flag01.tem");
     assert_eq!(layer.canvas_width, 1);
     assert_eq!(layer.canvas_height, 1);
     assert_eq!(layer.frames.len(), 2);
     assert_eq!(layer.shp_frames, 2);
-    assert_eq!(layer.palette, "unittem.pal");
-    assert_eq!(layer.frames[0].width, 1, "1x1 SHP keeps full-canvas blit size");
-    assert_eq!(layer.frames[0].height, 1);
+    assert_eq!(layer.palette, "isotem.pal");
     assert_eq!(layer.rate_ms, 200);
     assert_eq!(bank.frame_signature(0), bank.frame_signature(199));
     assert_ne!(bank.frame_signature(0), bank.frame_signature(200));
@@ -299,7 +338,7 @@ fn terrain_anim_bank_records_hit_and_paints_by_clock() {
 }
 
 #[test]
-fn animated_terrain_skips_shadow_half_frames() {
+fn looping_animated_terrain_skips_shadow_half_frames() {
     // 4 帧：主体绿/红 + 落影半幅（索引 1）。时钟若误用全 4 帧会在 400ms 画到落影蓝。
     let mut pal = solid_index_pal(5, 0, 63, 0);
     pal[6 * 3] = 63;
@@ -310,15 +349,13 @@ fn animated_terrain_skips_shadow_half_frames() {
     pal[1 * 3 + 2] = 63;
 
     let mut files = HashMap::new();
-    files.insert("art.ini".into(), b"[TIBTRE01]\nTheater=yes\n".to_vec());
-    files.insert(
-        "rules.ini".into(),
-        b"[TIBTRE01]\nIsAnimated=yes\nAnimationRate=3\nSpawnsTiberium=yes\n".to_vec(),
-    );
-    files.insert("unittem.pal".into(), pal);
-    files.insert("tibtre01.tem".into(), multi_frame_shp(&[5, 6, 1, 1]));
+    files.insert("art.ini".into(), b"[FLAG01]\nTheater=yes\n".to_vec());
+    files.insert("rules.ini".into(), b"[FLAG01]\nIsAnimated=yes\nAnimationRate=3\n".to_vec());
+    files.insert("isotem.pal".into(), pal);
+    files.insert("flag01.tem".into(), multi_frame_shp(&[5, 6, 1, 1]));
     let source = MapSource { files };
-    let map = tibtre_map();
+    let mut map = MapInfo::empty(GameEdition::Ra2, "t");
+    map.terrain_objects = vec![TerrainObject { x: 5, y: 0, name: "FLAG01".into() }];
 
     let bank = collect_terrain_anim_bank(&source, &map, "art.ini", "rules.ini");
     assert_eq!(bank.layers.len(), 1);
@@ -335,9 +372,9 @@ fn animated_terrain_skips_shadow_half_frames() {
 }
 
 #[test]
-fn spawns_tiberium_blits_full_canvas_with_cell_height_y() {
-    // 84×56 画布、子帧 (24,4) 的 1×1。完整画布偏移相对钻石中心为 (-42, -28-15)。
-    // 换算到 iso 原点：offset = (30-42, 15-28-15) = (-12, -28)。
+fn spawns_tiberium_blits_full_canvas_with_fa2_y_fudge() {
+    // 84×56 画布、子帧 (24,4) 的 1×1。完整画布相对钻石中心 (-42, -28-3)。
+    // 换算到 iso 原点：offset = (30-42, 15-28-3) = (-12, -16)。
     let mut data = Vec::new();
     data.extend_from_slice(&0u16.to_le_bytes());
     data.extend_from_slice(&84u16.to_le_bytes());
@@ -356,29 +393,33 @@ fn spawns_tiberium_blits_full_canvas_with_cell_height_y() {
 
     let mut files = HashMap::new();
     files.insert("art.ini".into(), b"[TIBTRE01]\nTheater=yes\n".to_vec());
-    files.insert("rules.ini".into(), b"[TIBTRE01]\nSpawnsTiberium=yes\n".to_vec());
+    files.insert(
+        "rules.ini".into(),
+        b"[TIBTRE01]\nIsAnimated=yes\nSpawnsTiberium=yes\n".to_vec(),
+    );
     files.insert("unittem.pal".into(), solid_index_pal(5, 63, 0, 0));
     files.insert("isotem.pal".into(), solid_index_pal(5, 0, 63, 0));
     files.insert("tibtre01.tem".into(), data);
     let source = MapSource { files };
     let map = tibtre_map();
 
-    let bank = collect_terrain_anim_bank(&source, &map, "art.ini", "rules.ini");
-    // 无 IsAnimated 时银行为空，改用直接叠画检查尺寸与落点。
-    assert!(bank.is_empty());
+    assert!(collect_terrain_anim_bank(&source, &map, "art.ini", "rules.ini").is_empty());
 
     let mut image = TerrainImage::blank(256, 256);
-    assert_eq!(paint_map_terrain_objects(&source, &map, &mut image, "art.ini", "rules.ini", clock(0)), 1);
+    assert_eq!(
+        paint_map_terrain_objects(&source, &map, &mut image, "art.ini", "rules.ini", TerrainPaintMode::StaticOnly),
+        1
+    );
 
     let (sx, sy) = ra_map::iso_to_screen(5, 0, 0);
     // 完整画布左上角 + 子帧 (24,4) → 不透明像素相对 iso 原点：
-    // (-12+24, -28+4) = (12, -24)
+    // (-12+24, -16+4) = (12, -12)
     let expect_x = (sx + 12 - image.origin_x) as u32;
-    let expect_y = (sy - 24 - image.origin_y) as u32;
+    let expect_y = (sy - 12 - image.origin_y) as u32;
     let w = image.image.width();
     let px = image.image.as_raw();
     let di = ((expect_y * w + expect_x) * 4) as usize;
     assert!(di + 3 < px.len(), "pixel index in bounds");
-    assert!(px[di + 3] > 0, "expected ore-tree pixel at full-canvas anchor ({expect_x},{expect_y})");
+    assert!(px[di + 3] > 0, "expected ore-tree pixel at full-canvas FA2 anchor ({expect_x},{expect_y})");
     assert!(px[di] > px[di + 1], "expected unittem red at ore-tree pixel");
 }
