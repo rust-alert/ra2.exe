27aefc5c61ba204a15f9d292815205aebed6bdd8
aster <aster@vers.site>
2016-09-07 20:06:52 +0000

    ✨ Expose sorted `numbered_pairs` for INI index slots

--- projects/engine/ra-assets/src/ini/numbered.rs
+++ projects/engine/ra-assets/src/ini/numbered.rs
@@ -3,18 +3,24 @@
 use super::document::{IniDocument, IniSection};
 use super::merge::{EntryMergePolicy, IniMergePolicy, LayeredIniView};
 
-/// 将节内编号键（`1=` / `2=` …）按数值序拼接值文本。
-pub fn concat_numbered_values(section: &IniSection) -> Option<String> {
+/// 收集节内编号键（`0=` / `1=` …），按数值升序返回 `(index, value)`。
+pub fn numbered_pairs(section: &IniSection) -> Vec<(u32, &str)> {
     let mut pairs: Vec<(u32, &str)> = Vec::new();
     for (k, v) in section.pairs() {
         if let Ok(n) = k.parse::<u32>() {
             pairs.push((n, v));
         }
     }
+    pairs.sort_by_key(|(n, _)| *n);
+    pairs
+}
+
+/// 将节内编号键（`1=` / `2=` …）按数值序拼接值文本。
+pub fn concat_numbered_values(section: &IniSection) -> Option<String> {
+    let pairs = numbered_pairs(section);
     if pairs.is_empty() {
         return None;
     }
-    pairs.sort_by_key(|(n, _)| *n);
     let mut out = String::new();
     for (_, v) in pairs {
         out.push_str(v);
--- projects/engine/ra-assets/src/lib.rs
+++ projects/engine/ra-assets/src/lib.rs
@@ -48,7 +48,8 @@ pub use image::{
 pub use ini::{
     EntryMergePolicy, FieldMergeOverrides, IniDeError, IniDocument, IniEntry, IniMergePolicy, IniSection, IniValue, LayeredIniView,
     LayeredSectionView, ResolvedIniValue, SourceId,
-    SourceSpan, collect_shp_refs, concat_numbered_values, from_layered_section, from_section, numbered_section_concat,
+    SourceSpan, collect_shp_refs, concat_numbered_values, from_layered_section, from_section, numbered_pairs,
+    numbered_section_concat,
 };
 pub use mix::{
     archive::{MixArchive, MixEntry},
