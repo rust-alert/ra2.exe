e93f70e2f0f51ac0ebbce06d6ed4ddad32a24353
aster <aster@vers.site>
2017-07-06 14:11:47 +0000

    🧪 Align options rail slot tests with keyboard network `main_menu`
    The options page rail is keyboard network and `main_menu`. Drop the stale accept cancel Back assertions.

--- projects/engine/ra-widgets/tests/widgets/skin/slots.rs
+++ projects/engine/ra-widgets/tests/widgets/skin/slots.rs
@@ -77,16 +77,16 @@ fn load_screen_exposes_retry_and_cancel_slots() {
 }
 
 #[test]
-fn options_rail_accept_cancel_main_menu() {
+fn options_rail_keyboard_network_main_menu() {
     let page = slots_for(OriginalScreen::Options).unwrap();
     let ids: Vec<_> = page.buttons.iter().map(|b| b.entry_id).collect();
-    assert_eq!(ids, ["accept", "cancel", "main_menu"]);
+    assert_eq!(ids, ["keyboard", "network", "main_menu"]);
     assert!(page.buttons[0].enabled);
-    assert!(matches!(page.buttons[0].action, MenuAction::OptionsAccept));
+    assert!(matches!(page.buttons[0].action, MenuAction::Noop));
     assert!(page.buttons[1].enabled);
-    assert!(matches!(page.buttons[1].action, MenuAction::OptionsCancel));
+    assert!(matches!(page.buttons[1].action, MenuAction::OpenNetwork));
     assert!(page.buttons[2].enabled);
-    assert!(matches!(page.buttons[2].action, MenuAction::Back));
+    assert!(matches!(page.buttons[2].action, MenuAction::OptionsAccept));
     assert!(page.has_any_asset_name());
     assert_eq!(page.background_shp, Some("mnscrnl.shp"));
     assert_eq!(page.background_pal, Some("shell.pal"));
